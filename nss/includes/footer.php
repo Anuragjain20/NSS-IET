5ea8326756bf8aadbaedcfe9c27c7631f626406f
pagare-mansee <64631013+pagare-mansee@users.noreply.github.com>
2020-09-02 11:16:09 +0530

    added comments

--- nss/includes/footer.php
+++ nss/includes/footer.php
@@ -1,61 +1,57 @@
 <footer>
   <div class="container-fluid mt-5" style="background-color:lightcyan">
       <div class="row">
-        <div class="col-sm-5">
-             <a href="http://www.ietdavv.edu.in/" data-toggle="tooltip" title="IET-DAVV website"><img class="foot-logo img-fluid float-left" src="images/logo/iet logo.png" alt= "davv logo"></a>
         
-            <a href="#" data-toggle="tooltip" title="NSS-DAVV website"> <img class="foot-logo img-fluid float-right" src="images/logo/nss logo.png " alt= "nss logo"> </a>
-            <h4 class="foot-heading">NSS-IET DAVV</h4>
-       
-            <br>
-
-            <div style="text-align: center;">
-                
-          
-             
-            <a href="mailto:nssietdavv@gmail.com" data-toggle="tooltip" title="Mail us" ><i ><button class="fa fa-envelope-o foot-icons foot-button" style="font-size:25px" ></button></i></a>
-         <a href="https://www.facebook.com/NSSIET" data-toggle="tooltip" title="Connect on facebook"><i><button class= "fa fa-facebook-official foot-icons foot-button " style="font-size:25px;" ></button></i></a>
-  <a href="https://www.youtube.com/channel/UCS3vch09BK4Zvf8GoIjlmMw/featured" data-toggle="tooltip" title="Watch us on Youtube"  ><i><button class="fa fa-youtube foot-icons foot-button" style="font-size:25px" ></button></i></a>
-          
-          </ul>
-          </div>
+        <div class="col-sm-5">
+          <!--logos  -->
+         <a href="http://www.ietdavv.edu.in/" data-toggle="tooltip" title="IET-DAVV website"><img class="foot-logo img-fluid float-left" src="images/logo/iet logo.png" alt= "davv logo"></a>
+        <a href="#" data-toggle="tooltip" title="NSS-DAVV website"> <img class="foot-logo img-fluid float-right" src="images/logo/nss logo.png " alt= "nss logo"> </a>
+             <!--heading  -->
+          <h4 class="foot-heading">NSS-IET DAVV</h4>
           <br>
-          <div style="text-align: center;">
-                
-           
-   
-            <a href="https://www.instagram.com/nss_ietchapter/" data-toggle="tooltip" title="With Instagram"  ><i><button class="fa fa-instagram foot-icons foot-button" style="font-size:25px" ></button> </i></a>
-          <a href=# data-toggle="tooltip" title="With Twitter"  ><i ><button class="fa fa-twitter foot-icons foot-button" style="font-size:25px" ></button></i></a>
-            <a href=# data-toggle="tooltip" title="With Wordpress"  ><i><button class="fa fa-wordpress foot-icons foot-button" style="font-size:25px" ></button></i></a>
-            
-            </div>
           
+          <!--social media icons-->
+            <div style="text-align: center;">
+              <a href="mailto:nssietdavv@gmail.com" data-toggle="tooltip" title="Mail us" ><i ><button class="fa fa-envelope-o foot-icons foot-button" style="font-size:25px" ></button></i></a>
+              <a href="https://www.facebook.com/NSSIET" data-toggle="tooltip" title="Connect on facebook"><i><button class= "fa fa-facebook-official foot-icons foot-button " style="font-size:25px;" ></button></i></a>
+              <a href="https://www.youtube.com/channel/UCS3vch09BK4Zvf8GoIjlmMw/featured" data-toggle="tooltip" title="Watch us on Youtube"  ><i><button class="fa fa-youtube foot-icons foot-button" style="font-size:25px" ></button></i></a>
+             </div>
+           <br>
+            <div style="text-align: center;">
+             <a href="https://www.instagram.com/nss_ietchapter/" data-toggle="tooltip" title="With Instagram"  ><i><button class="fa fa-instagram foot-icons foot-button" style="font-size:25px" ></button> </i></a>
+             <a href=# data-toggle="tooltip" title="With Twitter"  ><i ><button class="fa fa-twitter foot-icons foot-button" style="font-size:25px" ></button></i></a>
+             <a href=# data-toggle="tooltip" title="With Wordpress"  ><i><button class="fa fa-wordpress foot-icons foot-button" style="font-size:25px" ></button></i></a>
+           </div>    
+           <br>
       <br>
-      <br>
-          </div>
+            </div>
 
            <div class="col-sm-1"></div>
-          <div class="col-sm-5 "> 
-             
-              <h3 class="foot-heading"> Quick links</h3>
+        
+         <!--Quicklinks heading-->
+            <div class="col-sm-6 "> 
+            <h3 class="foot-heading"> Quick links</h3>
               <br>
+              
+           <!--Quicklinks-->
               <div class="row">
                   <div class="col-6 pr-5 ">
-              <ul style="list-style-type:none;float:right">
-                <li><a href="team.php"  class="foot-quicklinks">Team</a></li>
-               
-                <li style="margin-top:6px"><a href="contact-us.php" class="foot-quicklinks">Contact us</a></li>
-                <li style="margin-top:6px"><a href="#"class="foot-quicklinks">Developers</a></li>
-              </ul>
-              </div>
-              <div class="col-6 pl-3  ">
-                  <ul style="list-style-type:none;float:left">
-                    <li><a href="gallery.php"class="foot-quicklinks" >Gallery</a></li>
-                    <li style="margin-top:6px;"><a href="events.php" class="foot-quicklinks">Events</a></li>
-                    <li style="margin-top:6px"><a href="#" class="foot-quicklinks">Feedback</a></li>
-                  </ul>
-                  </div>
-              </div>
+                     <ul style="list-style-type:none;float:right">
+                        <li><a href="team.php"  class="foot-quicklinks">Team</a></li>
+                        <li style="margin-top:6px"><a href="contact-us.php" class="foot-quicklinks">Contact us</a></li>
+                        <li style="margin-top:6px"><a href="#"class="foot-quicklinks">Developers</a></li>
+                      </ul>
+                   </div>
+                  
+                <div class="col-6 pl-3  ">
+                    <ul style="list-style-type:none;float:left">
+                         <li><a href="gallery.php"class="foot-quicklinks" >Gallery</a></li>
+                          <li style="margin-top:6px;"><a href="events.php" class="foot-quicklinks">Events</a></li>
+                           <li style="margin-top:6px"><a href="#" class="foot-quicklinks">Feedback</a></li>
+                     </ul>
+                </div>
+                
+               </div>
             </div>
           </div>
       </div>
