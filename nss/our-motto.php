ee6e453d34d389bdc17ae6ee500b836db5b0803c
pagare-mansee <64631013+pagare-mansee@users.noreply.github.com>
2020-09-02 19:01:01 +0530

    added comments

--- nss/our-motto.php
+++ nss/our-motto.php
@@ -29,14 +29,22 @@
 
 
 <!--motto-->
+   <!--page-name-->
+   
 <div class="container-fluid pagename">MISSION</div>
+   
+   <!--We NSS family-->
+   
 <div class="container"  style="background-color:khaki;">
 <div class="container s-heading">We NSS Family</div>
 <p class=" container text-m"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    </p>
-    <br>
+</p>
+ <br>
 </div>
 <br>
+   
+   <!--AIM-->
+   
 <div class="container"  style="background-color:khaki;">
     <div class="container s-heading">Our Aim </div>
     <p class=" container text-m"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -45,23 +53,29 @@
     </div>
 </div>
 <br>
-    <div class="container"  style="background-color:khaki;">
+   
+   <!--WORKS-->
+  <div class="container"  style="background-color:khaki;">
         <div class=" s-heading"> Our Works </div>
         <br>
-    <div class="container">
+     
+     <!--health-->
+   <div class="container">
         <div class="row">
         <div class="col-md-4">
     <h4 class="t-heading"><i class="fa fa-plus-square" style="font-size:25px;color:red"></i> HEALTH</h4>
     <P class=" container text-m">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</P>
     <br> </div>
-
+      <!--awareness-->
+   
 <div class="col-md-4">
 <h4 class="t-heading"> <i class="fa fa-users" style="font-size:25px;color:blue"></i> AWARENESS</h4>
 <P class=" container text-m">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</P>  
 <br></div>
 
 
-
+        <!--Celebrations-->
+   
         <div class="col-md-4">
             <h4 class="t-heading"> <i class="fa fa-star"style="font-size:25px;color:gold" ></i> CELEBRATIONS</h4>
             <P class=" container text-m">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</P>
