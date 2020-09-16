d0d3c47c572fd213fac70c514867758e19629cbb
Anurag <anuragjain2rr@gmail.com>
2020-09-16 20:42:20 +0530

    footer updated

--- nss/includes/footer.php
+++ nss/includes/footer.php
@@ -1,69 +1,107 @@
-<footer>
-  <div class="container-fluid mt-5" style="background-color:lightcyan">
-      <div class="row">
-        <div class="col-sm-5">
-             <a href="http://www.ietdavv.edu.in/" data-toggle="tooltip" title="IET-DAVV website"><img class="foot-logo img-fluid float-left" src="images/logo/iet logo.png" alt= "davv logo"></a>
-        
-            <a href="#" data-toggle="tooltip" title="NSS-DAVV website"> <img class="foot-logo img-fluid float-right" src="images/logo/nss logo.png " alt= "nss logo"> </a>
-            <h4 class="foot-heading">NSS-IET DAVV</h4>
-       
-            <br>
-            <div style="text-align: center;">
-                
-          
-             
-            <a href="mailto:nssietdavv@gmail.com" data-toggle="tooltip" title="Mail us" ><i ><button class="fa fa-envelope-o foot-icons foot-button" style="font-size:25px" ></button></i></a>
-         <a href="https://www.facebook.com/NSSIET" data-toggle="tooltip" title="Connect on facebook"><i><button class= "fa fa-facebook-official foot-icons foot-button " style="font-size:25px;" ></button></i></a>
-  <a href="https://www.youtube.com/channel/UCS3vch09BK4Zvf8GoIjlmMw/featured" data-toggle="tooltip" title="Watch us on Youtube"  ><i><button class="fa fa-youtube foot-icons foot-button" style="font-size:25px" ></button></i></a>
-          
-          </ul>
-          </div>
-          <br>
-          <div style="text-align: center;">
-                
-           
-   
-            <a href="https://www.instagram.com/nss_ietchapter/" data-toggle="tooltip" title="With Instagram"  ><i><button class="fa fa-instagram foot-icons foot-button" style="font-size:25px" ></button> </i></a>
-          <a href=# data-toggle="tooltip" title="With Twitter"  ><i ><button class="fa fa-twitter foot-icons foot-button" style="font-size:25px" ></button></i></a>
-            <a href=# data-toggle="tooltip" title="With Wordpress"  ><i><button class="fa fa-wordpress foot-icons foot-button" style="font-size:25px" ></button></i></a>
-            
-            </div>
-          
-      <br>
-      <br>
-          </div>
-           <div class="col-sm-1"></div>
-          <div class="col-sm-5 "> 
-             
-              <h3 class="foot-heading"> Quick links</h3>
-              <br>
-              <div class="row">
-                  <div class="col-6 pr-5 ">
-              <ul style="list-style-type:none;float:right">
-                <li><a href="team.php"  class="foot-quicklinks">Team</a></li>
-               
-                <li style="margin-top:6px"><a href="contact-us.php" class="foot-quicklinks">Contact us</a></li>
-                <li style="margin-top:6px"><a href="#"class="foot-quicklinks">Developers</a></li>
-              </ul>
-              </div>
-              <div class="col-6 pl-3  ">
-                  <ul style="list-style-type:none;float:left">
-                    <li><a href="gallery.php"class="foot-quicklinks" >Gallery</a></li>
-                    <li style="margin-top:6px;"><a href="events.php" class="foot-quicklinks">Events</a></li>
-                    <li style="margin-top:6px"><a href="#" class="foot-quicklinks">Feedback</a></li>
-                  </ul>
-                  </div>
-              </div>
-            </div>
-          </div>
-      </div>
-      <div style="background-color:#123c69;color:white;text-align:center">
-        Developed and managed by NSS IET-DAVV <br> 
-        ©Copyright 2020
+  <!--footer-->
+  <footer>
+    <div class="container-fluid mt-5 footermain">
+      <div class="row pt-3" >
+        <div class="col-lg-3 ">
+  
+          <table align="center">
+            <tbody><tr align="center">
+              <td><a href="#"><img src="images/logo/nss logo.png" class=" footlogo" ></a></td>
+            </tr>
+            <tr align="center " valign="bottom">
+              <td><h4 class="mt-3">NSS, IET-DAVV</h4></td>
+            </tr>
+  
+          </tbody></table>
+  
+          <hr class="hrstyle">
+  
+          <table class="footcol-1 text-center">
+            <tbody><tr>
+              <td width="58px" valign="top">
+                <i class="fa fa-map-marker-alt "></i>
+              </td>
+              <td class="tdstyle">
+                <span>IET-DAVV,Khandwa Road,Indore,452017(M.P.)</span>
+              </td>
+            </tr>
+  
+            <tr class="trstyle">
+              <td>
+                <i class="fas fa-phone-volume"></i>
+              </td>
+              <td>
+  
+                <span>0731-2361116 / 2361117<span>
+              </td>
+            </tr>
+            <tr class="trstyle">
+              <td>
+                <i class="fas fa-envelope-open"></i>
+              </td>
+              <td>
+                <span>nssietdavv@gmail.com</span>
+              </td>
+            </tr>
+          </tbody></table>
         </div>
-    
-    
-      </footer>
+        <div class="col-lg-4 my-5 ">
+          <h4 align="center ">
+            QUICK LINKS
+          </h4>
+          <div class="row my-5 ">
+            
+            <div class=" footcol-2"> 
+              <ul class="footcol-21 ">
+                <a href="team.php" class="footlinks"><li>Team</li></a>
+                <a href="http://www.ietdavv.edu.in/" class="footlinks"><li>Davv</li></a>
+                <a href="gallery.php" class="footlinks"><li>Gallery</li></a>
+              </ul>
+            </div>
+            <div class="footcol-2 " >
+              <ul class="footcol-22 " >
+                <a href="events.php" class="footlinks"><li>Events</li></a>
+                <a href="#" class="footlinks"><li>Feedback</li></a>
+                <a href="contact-us.php" class="footlinks"><li>Contact Us</li></a>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+  
+        <div class="col-lg-5 ">
+          <div class="row my-3 "><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.2652289337734!2d75.87767901496154!3d22.68116838512704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fddd986e5571%3A0x115bc66d83167fc9!2sInstitute%20of%20Engineering%20%26%20Technology%20(DAVV)!5e0!3m2!1sen!2sin!4v1596567842144!5m2!1sen!2sin"  frameborder="0" allowfullscreen="" class="img-thumbnail iframestyle"></iframe></div>
+          <div class="row  my-3">
+            <div class="col-12 text-center iconsfoot">
+              <a href=""><i class='fab fa-twitter' style="color:#0ac3ff;"></i></a>
+              <a href="https://www.instagram.com/nss_ietchapter/"><i class='fab fa-instagram ' style="color: #d051a3;"></i></a>
+              <a href="https://www.facebook.com/NSSIET"><i class='fab fa-facebook ' style="color: darkblue;"></i></a>
+              <a href="#"><i class='fab fa-wordpress' style="color: blue;"></i></a>
+              <a href="https://www.youtube.com/channel/UCS3vch09BK4Zvf8GoIjlmMw/featured"><i class='fab fa-youtube' style="color: red;"></i></a>
+              
+             
+            </div>
+          </div>
+          <div class="row my-3">
+            <div class="col-12 ">
+              <div class=" h4 text-center "><a href="#" class="developers">Developers</a></div>
+
+            </div>
+
+          </div>
+          
+  
+        </div>
+  
+      </div>
+      
+    </div>
+    <div class="copyright">
+      Developed and managed by NSS IET-DAVV <br> 
+      ©Copyright 2020
+      </div>
+  </footer>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>      
