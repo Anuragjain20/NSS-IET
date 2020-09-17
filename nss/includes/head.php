f5461bdc2ba6c2e50e998fc6c957d77807f8038a
Anurag <anuragjain2rr@gmail.com>
2020-09-17 15:52:22 +0530

    update head and foot

--- nss/includes/head.php
+++ nss/includes/head.php
@@ -12,4 +12,4 @@
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
        <link rel="icon" href="images/logo/nss logo.png" type="image/icon type">
-    <title>National Service Scheme,IET-DAVV</title>
+    <title>National Service Scheme,IET-DAVV,Indore</title>
