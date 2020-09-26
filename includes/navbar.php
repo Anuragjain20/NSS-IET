778e678f18f7f102d7b5097d5c2ec5ce0d19f16c
Sumit Singh <singh.sumit684@gmail.com>
2020-09-26 15:26:12 +0530

    Changed folder

---
+++ includes/navbar.php
@@ -0,0 +1,63 @@
+        <nav class="navbar navbar-expand-lg navbar-dark sticky-top  navicustom">
+          <div class="container-fluid">
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarlinks" aria-controls="navbarlinks" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <!--links of navbar in left-->
+            <div class="collapse navbar-collapse" id="navbarlinks">
+              <ul class="navbar-nav mr-auto ">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                
+                <!-- About US {Dropdown} -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                      About Us
+                    </a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="our-motto.php">Our motto </a>
+                      <a class="dropdown-item" href="team.php">Our Team</a>
+                      <a class="dropdown-item" href="#">NSS Camps</a>
+                      <a class="dropdown-item" href="messages.php">Messages</a>
+                    </div>
+                  </li>
+            
+                <!-- Downloads {Dropdown} -->
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Downloads
+                  </a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">NSS Diary </a>
+                    <a class="dropdown-item" href="#">Camp Guidelines</a>
+                    <a class="dropdown-item" href="#">NSS Geet</a>
+                    <a class="dropdown-item" href="#">Calender</a>
+                    <a class="dropdown-item" href="#">Reports</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="events.php">Events</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">Gallery</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact-us.php">Contact Us</a>
+                  </li>
+              </ul>
+              <!--navbar  button on right-->
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#"><button type="button" class="btn btn-danger">Login</button></a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#"><button type="button" class="btn btn-danger">Register</button></a>
+                </li>
+            </ul>
+            </div>
+    
+    
+          </div>
+        </nav>
