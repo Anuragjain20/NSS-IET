9cd765c1e6b3416ee5ee234060f4b7726477707e
Sumit Singh <singh.sumit684@gmail.com>
2020-09-29 03:06:00 +0530

    Added AOS Animation
    Added Animations in Index page
    Changed youtube embeded link on Video 3

--- includes/event-index.php
+++ includes/event-index.php
@@ -1,30 +1,30 @@
 
-<h4 class="headingshp my-5">Events</h4>
+<h4 class="headingshp my-5"  data-aos="fade-in" >Events</h4>
 
-<div class="container-fluid background" >
-        <div class="row my-5">
-          <!-- celebration-->
-         <div class="col-sm-4 my-4">
-            <img src="images/event-section-homepage/celebration2.jpg"class="image" style="width:50%">
-            <div class="middle">
-                <a href="events.php#celebration"><button class="text">CELEBRATIONS</button></a>
-            </div>
-        </div>
-                 <!-- Health-->
-        <div class="col-sm-4 my-4">
-
-            <img src="images/event-section-homepage/health2.jpg" class="image" style="width:50%" >
-            <div class="middle">
-                <a href="events.php#health"><button class="text">HEALTH</button></a>
-            </div>
-        </div>
-                 <!-- Awareness-->
-      <div class="col-sm-4 my-4">
-            
-            <img src="images/event-section-homepage/awareness 2.png" class="image" style="width:50% ">
-            <div class="middle">
-              <a href="events.php#awareness"><button class="text">AWARENESS PROGRAMS</button></a>
-             </div>
+<div class="container-fluid background"  data-aos="fade-in">
+  <div class="row my-5">
+    <!-- celebration-->
+    <div class="col-sm-4 my-4" data-aos="fade-right">
+      <img src="images/event-section-homepage/celebration2.jpg"class="image" style="width:50%">
+      <div class="middle">
+        <a href="events.php#celebration"><button class="text">CELEBRATIONS</button></a>
       </div>
     </div>
- </div>
+    <!-- Health-->
+    <div class="col-sm-4 my-4" data-aos="fade-up">
+
+      <img src="images/event-section-homepage/health2.jpg" class="image" style="width:50%" >
+      <div class="middle">
+        <a href="events.php#health"><button class="text">HEALTH</button></a>
+      </div>
+    </div>
+    <!-- Awareness-->
+    <div class="col-sm-4 my-4" data-aos="fade-left">
+
+      <img src="images/event-section-homepage/awareness 2.png" class="image" style="width:50% ">
+      <div class="middle">
+        <a href="events.php#awareness"><button class="text">AWARENESS PROGRAMS</button></a>
+      </div>
+    </div>
+  </div>
+</div>
--- includes/youtubeembed.php
+++ includes/youtubeembed.php
@@ -1,24 +1,24 @@
 
 
-<div class="container-fluid my-5">
+<div class="container-fluid my-5" >
   <div class="row ">
-      <div class="col-12 my-3"><p class="h2 text-center headingshp" >Glimpses</p></div>
+    <div class="col-12 my-3" data-aos="fade-in"><p class="h2 text-center headingshp" >Glimpses</p></div>
   </div> 
-  <div class="row my-5 text-center" >
-      <div class="col-lg-4 col-12  my-4">
-              <div class="container-fluid">
-              <iframe class="youtube-em" src="https://www.youtube.com/embed/mQgo6Oz4sxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+  <div class="row my-5 text-center"  data-aos="fade-up">
+    <div class="col-lg-4 col-12  my-4">
+      <div class="container-fluid">
+        <iframe class="youtube-em" src="https://www.youtube.com/embed/mQgo6Oz4sxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class="col-lg-4 col-12  my-4">
-          <div class="container-fluid">
-              <iframe class="youtube-em" src="https://www.youtube.com/embed/w5c3uY5Xmrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+    </div>
+    <div class="col-lg-4 col-12  my-4" data-aos="fade-up">
+      <div class="container-fluid">
+        <iframe class="youtube-em" src="https://www.youtube.com/embed/w5c3uY5Xmrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div class="col-lg-4 col-12 my-4">
-          <div class="container-fluid">
-              <iframe class="youtube-em" src="https://www.youtube.com/embed/-mnordDSSR4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+    </div>
+    <div class="col-lg-4 col-12 my-4" data-aos="fade-up">
+      <div class="container-fluid">
+        <iframe class="youtube-em"  src="https://www.youtube.com/embed/L1D_JsoQhTg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
+    </div>
   </div>
 </div>
