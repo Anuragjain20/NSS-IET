e8fb59b4fead0e9950edc098877c82a0dd1bc2b4
pagare-mansee <64631013+pagare-mansee@users.noreply.github.com>
2020-10-02 11:00:25 +0530

    changed the contents of Activities

---
+++ nss/events.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    
+    <!--external css-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link  rel="stylesheet" href="css/events.css ">
+    
+       <!--head php-->
+       <?php include 'includes/head.php';?>
+    
+</head>
+<body>
+  
+     	  <!-- Header -->
+         <?php include 'includes/header.php';?>
+
+
+
+
+        <!--nav-bar-->
+         <?php include 'includes/navbar.php';?>
+
+<!--events section-main-->
+<div class="event-mainhead text-center  my-5">Activities</div>
+
+<!--celebrations-->
+<div class="container-fluid my-3" id="celebration">
+  <div class="text-center h1 py-5 subheadings" >Celebrations</div>
+
+  <!--first blog-->
+  <div class="blog-card my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic1.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+  
+      <h1>Guru Purnima</h1>
+      <p>The auspicious day Guru Purnima is celebrated every year to pay tribute to the selflessness and dedication of the Gurus. The students show their gratefulness towards their teachers through poems songs cards and videos. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--second blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic2.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+     
+      <h1> Photo Walk</h1>
+      <p>Photo Walk is organised one or two days before the NSS day to give students an opportunity to showcase their creativity through photography. It makes them to see the world around them with a different view. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--third blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic3.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+    
+      <h1>NSS Day</h1>
+      <p>NSS Day is observed every year on 24th September and each year it is celebrated much enthusiastically and uniquely. It witnesses major public events like Nukkad Natak, visit to villages, interaction with public and much more. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--fourth blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic2.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <!-- <h1>Heading</h1>-->
+      <h1> Gandhi Jayanti </h1>
+      <p>Along with our national festivals viz Republic Day and Independence Day, we celebrate Gandhi Jayanti with full devotion. Workshops and activities related to cleanliness along with competitions like essay writing, slogan writing, poster making and so on are organised to remember him. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+</div>
+<!--health-->
+<div class="container-fluid my-5" id="health">
+  <div class="text-center h1 py-5 subheadings">Health</div>
+
+  <!--first blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic1.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1> Blood Donation camp</h1>
+      
+      <p>We believe that every blood Donor is a life saver and following this one day blood donation camp is organised every year in the month of January, during the National Youth week, in collaboration with any steamed local Hospital. Every year we succeed in contributing a good amount of blood to the blood bank. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--second blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic1.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1>Health Check-up Camp</h1>
+     
+      <p>Health is wealth and we are much concerned about this. One day health check-up camp is organised every year in collaboration with any reputed Hospital in the campus. This check-up includes check up of blood pressure, sugar level and eye sight and providing health Advises regarding the same 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--third blog-->
+  <div class="blog-card  my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic2.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1> International Yoga Day</h1>
+      
+      <p>We celebrate 21st June i.e. International Yoga Day with full spirit and enthusiasm every year. We celebrated a virtual Yoga Day amid COVID-19 by organising a live session Yoga with family and practicing the same from home. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  
+</div>
+
+<!--awareness-->
+<div class="container-fluid my-5" id="awareness">
+  <div class="text-center h1 py-5 subheadings">Awareness</div>
+
+  <!--first blog-->
+  <div class="blog-card  my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic1.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1>World Population Day</h1>
+     <p>World Population Day is observed on 11th July. NSS Volunteers make their contribution by spreading awareness among the public about the growing population issues through rallies and posters. 
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--second blog-->
+  <div class="blog-card alt my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic2.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1> National Youth Week </h1>
+      
+      <p>Swami Vivekananda's birthday on January 12 is celebrated as National Youth Day and the week commencing from that day is known as the National Youth Week.To remember him and his teachings seminars, art galleries and book-fairs are organised along with several activities throughout the week
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+  <!--third blog-->
+  <div class="blog-card  my-5">
+    <section class="meta">
+      <div class="photo-blog" style="background-image: url(images/events/pic3.jpg);">
+  
+      </div>
+      <ul class="details">
+        <li class="author"><a href="#">Name</a></li>
+        <li class="date">August 12, 2020</li>
+      </ul>
+    </section>
+  
+    <section class="description">
+      <h1>Heading</h1>
+      <h2>Subheading</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Cum vel aspernatur est pariatur beatae deserunt odio
+         enim eaque adipisci doloribus, esse laudantium unde nobis fugit cumque
+          voluptatibus suscipit. Provident, sunt.
+      </p>
+      <p class="read-more">
+        <a href="#">Read more</a>
+      </p>
+    </section>
+  </div>
+</div>
+
+
+
+
+       <!--footer-->
+       <?php include 'includes/footer.php';?>
+
+
+
+
+</body>
+</html>
