03f525d3c815940c49818af8000559a6fd41e23c
Anurag <anuragjain2rr@gmail.com>
2020-10-03 20:10:53 +0530

    updated font

--- includes/head.php
+++ includes/head.php
@@ -16,4 +16,7 @@
        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
        <link rel="icon" href="images/logo/nss logo.png" type="image/icon type">
        <link rel="stylesheet" href="css/about_us_home.css">
+       <style>body{    font-family: 'Montserrat', sans-serif;
+
+       }</style>
     <title>National Service Scheme,IET-DAVV</title>
