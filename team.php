0c114072f0a7dec0738ddcdfa436ecf5fd8b71fb
pagare-mansee <64631013+pagare-mansee@users.noreply.github.com>
2020-10-04 22:12:15 +0530

    modified team page

--- team.php
+++ team.php
@@ -28,119 +28,173 @@
     <div class="heading-team my-5">
      Current Team
     </div>
-          
-          <!--- Faculties-->
-    <div class="container">
-        <div class="container subheading">Faculty Advisers</div>
-          <br>
-        <div class="card-deck">
-            <div class="card">
-        <img src="images/Team/image.jfif" alt="image" class="img-p">
-        <div class="card-body">
-            <h4 class="card-title">John Doe</h4>
-            <p class="card-text"> John Doe is an architect and engineer</p>
-            <a href="#">abc@xyz.com</a>
-          </div>
-            </div>
-            <div class="card ">
-        <img src="images/Team/image.jfif" alt="image" class="img-p">
-        <div class="card-body">
-            <h4 class="card-title">John Doe</h4>
-            <p class="card-text"> John Doe is an architect and engineer</p>
-            <a href="#">abc@xyz.com</a>
-          </div>
-            </div>
-            <div class="card">
-            <img src="images/Team/image.jfif" alt="image" class="img-p">
-            <div class="card-body">
-                <h4 class="card-title">John Doe</h4>
-                <p class="card-text"> John Doe is an architect and engineer</p>
-                <a href="#">abc@xyz.com</a>
-              </div>
-                </div>
-        </div>
+
+    <!--- patron-->
+ <div class="container subheading">Patron</div>
+ <br>
+ 
+  <div class="container">
+ 
+    <div class="row">
+      <div class=col-md-3></div>
+      <div class="col-md-6 mt-3"style="text-align: center;">
+ <div class="card" style="background-color: rgb(240, 232, 232);text-align:center" >
+
+  <img src="images/Team/image.jfif" alt="image2" class="img-s">
+  <div class="card-body">
+      <h4 class="card-title">Name</h4>
+      <h6 class="card-text"> Designation</h6>
+      <a href="#">abc@xyz.com</a>
+    </div>
+    </div>
+    </div>
+    <div class=col-md-3></div>
+    </div>
  </div>
-          
-          <!---head- Volunteers-->
+ <br>
+
+  <!--- Program officers-->
+ <div class="container subheading">Program Officers</div>
+    <br>
     <div class="container">
- <div class="container subheading">Head Volunteers</div>
-          <br>
-          <div class="card-deck">
-            <div class="card">
-        <img src="images/Team/image2.jfif" alt="image2" class="img-p">
+ 
+          <div class="row">
+            <div class="col-md-6 mt-3"style="text-align: center;">
+          
+            <div class="card" style="background-color: rgb(240, 232, 232);">
+
+        <img src="images/Team/image.jfif" alt="image2" class="img-d">
         <div class="card-body">
-            <h4 class="card-title">John Doe</h4>
-            <p class="card-text"> John Doe is an architect and engineer</p>
+            <h4 class="card-title">Name</h4>
+            <h6 class="card-text"> Designation</h6>
             <a href="#">abc@xyz.com</a>
           </div>
+          </div>
             </div>
-            <div class="card">
-            <img src="images/Team/image2.jfif" alt="image2" class="img-p">
-            <div class="card-body">
-                <h4 class="card-title">John Doe</h4>
-                <p class="card-text"> John Doe is an architect and engineer</p>
-                <a href="#">abc@xyz.com</a>
-              </div>
-                </div>
-                <div class="card">
-                <img src="images/Team/image2.jfif" alt="image2" class="img-p">
+          
+          
+           
+                <div class="col-md-6 mt-3"style="text-align: center;">
+                <div class="card" style="background-color: rgb(240, 232, 232);">
+                <img src="images/Team/image.jfif" alt="image2" class="img-d">
                 <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text"> John Doe is an architect and engineer</p>
+                    <h4 class="card-title">Name</h4>
+                    <h6 class="card-text"> Designation</h6>
                     <a href="#">abc@xyz.com</a>
                   </div>
                     </div>
+                  </div>
+                </div>
    
 </div>
 </div>
+
 <br>
-<div class="container">
-<div class="card-deck">
-    <div class="card ">
-        <img src="images/Team/image2.jfif" alt="image2" class="img-p">
+
+          
+          <!--- Faculties-->
+          <div class="container subheading">Faculty Advisers</div>
+          <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mt-3" style="text-align: center;">
+         
+          <div class="card" style="background-color: rgb(240, 232, 232);">
+        <img src="images/Team/image.jfif" alt="image" class="img-fa">
+        <div class="card-body" >
+         <h4 class="card-title">Name</h4>
+         <h6 class="card-text"> Designation</h6>
+            <a href="#">abc@xyz.com</a>
+        </div>
+           </div>
+        </div>
+        
+           <div class="col-md-4 mt-3 "  style="text-align: center;">
+            
+            <div class="card " style="background-color: rgb(240, 232, 232);">
+            <img src="images/Team/image.jfif" alt="image" class="img-fa">
+            <div class="card-body">
+             <h4 class="card-title">Name</h4>
+             <h6 class="card-text"> Designation</h6>
+                <a href="#">abc@xyz.com</a>
+            </div>
+               </div>
+               </div>
+           
+               
+               <div class="col-md-4 mt-3" style="text-align: center;">
+                <div class="card" style="background-color: rgb(240, 232, 232);">
+                <img src="images/Team/image.jfif" alt="image" class="img-fa">
+                <div class="card-body">
+                 <h4 class="card-title">Name</h4>
+                    <h6 class="card-text"> Designation</h6>
+                    <a href="#">abc@xyz.com</a>
+                </div>
+                   </div>
+                   </div>
+      </div>
+      <br>
+          
+          <!---head- Volunteers-->
+
+          <div class="container subheading">Head Volunteers</div>
+    <br>
+    <div class="container">
+ 
+          <div class="row">
+            <div class="col-md-6 mt-3"style="text-align: center;">
+          
+            <div class="card" style="background-color: rgb(240, 232, 232);">
+
+        <img src="images/Team/image2.jfif" alt="image2" class="img-d">
         <div class="card-body">
-            <h4 class="card-title">John Doe</h4>
-            <p class="card-text"> John Doe is an architect and engineer</p>
+            <h4 class="card-title">Name</h4>
+            <h6 class="card-text"> Designation</h6>
             <a href="#">abc@xyz.com</a>
           </div>
+          </div>
             </div>
-            <div class=" card">
-                <img src="images/Team/image2.jfif" alt="image2" class="img-p">
+          
+          
+           
+                <div class="col-md-6 mt-3"style="text-align: center;">
+                <div class="card" style="background-color: rgb(240, 232, 232);">
+                <img src="images/Team/image2.jfif" alt="image2" class="img-d">
                 <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text">John Doe is an architect and engineer</p>
+                    <h4 class="card-title">Name</h4>
+                    <h6 class="card-text"> Designation</h6>
                     <a href="#">abc@xyz.com</a>
                   </div>
-                    </div><div class="card">
-                        <img src="images/Team/image2.jfif" alt="image2" class="img-p">
-                        <div class="card-body">
-                            <h4 class="card-title">John Doe</h4>
-                            <p class="card-text"> John Doe is an architect and engineer</p>
-                            <a href="#">abc@xyz.com</a>
-                          </div>
-                            </div>
+                    </div>
+                  </div>
+                </div>
+   
 </div>
 </div>
+
 <br>
 <br>
+
+    
+
           
          <!---button for  all volunteers list --> 
-<div style="text-align: center" class="conatiner bg-light"  >
-    <a href="#"><button class="vbutton"><b><i>Volunteers </i></b></button></a>  
+<div style="text-align: center" class="conatiner "  >
+    <a href="#"><button type="button" class="btn btn-danger btn-lg"><b><i>Volunteers </i></b></button></a>  
     </div>
       <br>
       <br>
           
           <!---buttons for teams of different years --> 
-    <div class="conatiner bg-light"style="text-align: center"  >
+    <div class="conatiner"style="text-align: center"  >
         
-    <a href="#"><button class="button-team">Current</button></a>
+    <a href="#"><button type="button" class="btn btn-danger btn-lg">Current</button></a>
             
-    <a href="#"><button class="button-team">2018-2019</button></a>
+    <a href="#"><button type="button" class="btn btn-danger btn-lg">2018-2019</button></a>
                         
-     <a href="#"><button class="button-team">2017-2018</button></a>
+     <a href="#"><button type="button" class="btn btn-danger btn-lg">2017-2018</button></a>
                                     
-     <a href="#"><button class="button-team">2016-2017</button></a>
+     <a href="#"><button type="button" class="btn btn-danger btn-lg">2016-2017</button></a>
      
     </div>
     <br>
@@ -157,7 +211,7 @@
 <div class="container">
     
     <div class="card-deck">
-    <div class="card bg-light ">
+    <div class="card bg-light">
     <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
   
     
@@ -166,14 +220,14 @@
       <div class="cardcontent">Volunteers</div>
         </div>
     </div>
-    <div class=" card bg-light">
+    <div class=" card bg-light" >
         <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
         <div class="card-body">
         <div class="cardcontent"><b>1300 </b></div>
         <div class="cardcontent">Facebook Followers</div>
             </div>
         </div>
-        <div class="card bg-light">
+        <div class="card bg-light" >
             <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
             <div class="card-body">
               <div class="cardcontent"><b>100 </b></div>
