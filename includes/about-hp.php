9c4eee4f9dc31c823d4a3dfcbdb1a9ebe2c132ef
Sumit Singh <singh.sumit684@gmail.com>
2020-10-05 20:38:18 +0530

    Change in Vision Mission Background

--- includes/about-hp.php
+++ includes/about-hp.php
@@ -1,24 +1,30 @@
-
-<div class=" my-5">
-<div class="parallax  spad">
-
-<div class="container">
-
+<section class="enroll-section spad parallax">
+  <div class="container">
    <div class="row">
-        <div class="col-md-6 mx-2" >
-            <div class="row">
-                 <div class="col-12 htext "><h3 data-aos="fade-down" data-aos-duration="1000">MOTTO</h3> 
-                  <p class="dtext" data-aos="fade-up" data-aos-duration="1000">With this motto of NSS "Not Me But You", the program aims to instill the idea of social welfare in students and inspire them to provide service to society without bias. NSS volunteers work to ensure that everyone who is needy gets help to enhance their standard of living and lead a life of dignity.</p><br> </div>
-                   <div class="col-12 htext"> <h3 data-aos="fade-down" data-aos-duration="1000">MISSION</h3>
-                    <p class="dtext" data-aos="fade-up" data-aos-duration="1000"> To provide a nurturing ground for holistic and integral development of future citizens who are persons of  5C's - Conscience,  Character, Compassion, Commitment and Competence.</p> <br></div>
-                    <div class="col-12 htext "><h3 data-aos="fade-up">VISION</h3> 
-                     <p class="dtext" data-aos="fade-down" data-aos-duration="1000">Spirit lead ambassadors for a better world of equality, harmony and excellence.</p> <br></div>
-
-              </div>
-          </div>
-
+    <div class="col-md-6 mx-2" >
+      <div class="row">
+       <div class="col-12 htext ">
+        <h3 data-aos="fade-down" data-aos-duration="1000">MOTTO</h3> 
+        <p class="dtext" data-aos="fade-up" data-aos-duration="1000">
+          With this motto of NSS "Not Me But You", the program aims to instill the idea of social welfare in students and inspire them to provide service to society without bias. NSS volunteers work to ensure that everyone who is needy gets help to enhance their standard of living and lead a life of dignity.
+        </p>
+        <br> 
       </div>
-
+      <div class="col-12 htext">
+       <h3 data-aos="fade-down" data-aos-duration="1000">MISSION</h3>
+       <p class="dtext" data-aos="fade-up" data-aos-duration="1000"> 
+         To provide a nurturing ground for holistic and integral development of future citizens who are persons of  5C's - Conscience,  Character, Compassion, Commitment and Competence.
+       </p>
+       <br>
+     </div>
+     <div class="col-12 htext "><h3 data-aos="fade-up">VISION</h3> 
+       <p class="dtext" data-aos="fade-down" data-aos-duration="1000">
+         Spirit lead ambassadors for a better world of equality, harmony and excellence.
+       </p>
+       <br>
+     </div>
+   </div>
+ </div>
 </div>
 </div>
-</div>
+</section>
