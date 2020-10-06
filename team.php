314f25a8814361a57a70c8c34cac7d75326aa427
Sumit Singh <singh.sumit684@gmail.com>
2020-10-06 10:57:18 +0530

    Team Page updated

--- team.php
+++ team.php
@@ -1,248 +1,248 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>    
-        <!--team css-->
-        <link rel="stylesheet" href="css/team.css">
+  <!--team css-->
+  <link rel="stylesheet" href="css/team.css">
 
-        <!--head php-->
-       <?php include 'includes/head.php';?>
+  <!--head php-->
+  <?php include 'includes/head.php';?>
 
 
 </head>
-  <body>
+<body>
 
 
-       	  <!-- Header -->
-           <?php include 'includes/header.php';?>
-
-
-
-
-          <!--nav-bar-->
-         <?php include 'includes/navbar.php';?>
+  <!-- Header -->
+  <?php include 'includes/header.php';?>
 
 
 
 
-      <!--- Team-->
-    <div class="heading-team my-5">
-     Current Team
-    </div>
+  <!--nav-bar-->
+  <?php include 'includes/navbar.php';?>
 
-    <!--- patron-->
+
+
+
+  <!--- Team-->
+  <div class="heading-team my-5">
+   Current Team
+ </div>
+
+ <!--- patron-->
  <div class="container subheading">Patron</div>
  <br>
  
-  <div class="container">
- 
-    <div class="row">
-      <div class=col-md-3></div>
-      <div class="col-md-6 mt-3"style="text-align: center;">
- <div class="card" style="background-color: rgb(240, 232, 232);text-align:center" >
+ <div class="container">
 
-  <img src="images/Team/image.jfif" alt="image2" class="img-s">
-  <div class="card-body">
-      <h4 class="card-title">Name</h4>
-      <h6 class="card-text"> Designation</h6>
-      <a href="#">abc@xyz.com</a>
-    </div>
-    </div>
-    </div>
+  <div class="row">
     <div class=col-md-3></div>
+    <div class="col-md-6 mt-3"style="text-align: center;">
+     <div class="card" style="background-color: rgb(240, 232, 232);text-align:center" >
+
+      <img src="images/Team/sanjiv.jpg" alt="Dr. Sanjiv Tokekar" class="img-s">
+      <div class="card-body">
+        <h4 class="card-title">Dr. Sanjiv Tokekar</h4>
+        <h6 class="card-text"> Director IET-DAVV</h6>
+        <a href="mailto:stokekar@ietdavv.edu.in">stokekar@ietdavv.edu.in</a>
+      </div>
     </div>
- </div>
- <br>
+  </div>
+  <div class=col-md-3></div>
+</div>
+</div>
+<br>
 
-  <!--- Program officers-->
- <div class="container subheading">Program Officers</div>
-    <br>
-    <div class="container">
- 
-          <div class="row">
-            <div class="col-md-6 mt-3"style="text-align: center;">
-          
-            <div class="card" style="background-color: rgb(240, 232, 232);">
+<!--- Program officers-->
+<div class="container subheading">Program Officers</div>
+<br>
+<div class="container">
 
-        <img src="images/Team/image.jfif" alt="image2" class="img-d">
+  <div class="row">
+    <div class="col-md-6 mt-3"style="text-align: center;">
+
+      <div class="card" style="background-color: rgb(240, 232, 232);">
+
+        <img src="images/Team/ssk.jpg" alt="image2" class="img-d">
         <div class="card-body">
-            <h4 class="card-title">Name</h4>
-            <h6 class="card-text"> Designation</h6>
-            <a href="#">abc@xyz.com</a>
-          </div>
-          </div>
-            </div>
-          
-          
-           
-                <div class="col-md-6 mt-3"style="text-align: center;">
-                <div class="card" style="background-color: rgb(240, 232, 232);">
-                <img src="images/Team/image.jfif" alt="image2" class="img-d">
-                <div class="card-body">
-                    <h4 class="card-title">Name</h4>
-                    <h6 class="card-text"> Designation</h6>
-                    <a href="#">abc@xyz.com</a>
-                  </div>
-                    </div>
-                  </div>
-                </div>
-   
+          <h4 class="card-title">Prof. Shailendra Singh Khinchi</h4>
+          <h6 class="card-text"> Faculty, Applied Science Department</h6>
+          <a href="mailto:skhinchi@ietdavv.edu.in">skhinchi@ietdavv.edu.in</a>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-md-6 mt-3"style="text-align: center;">
+      <div class="card" style="background-color: rgb(240, 232, 232);">
+        <img src="images/Team/suwarna.jpg" alt="image2" class="img-d">
+        <div class="card-body">
+          <h4 class="card-title">Dr. Suwarna Torgal</h4>
+          <h6 class="card-text"> Faculty, Mechanical Engineering Department</h6>
+          <a href="mailto:storgal@ietdavv.edu.in">storgal@ietdavv.edu.in</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 </div>
 
 <br>
 
-          
-          <!--- Faculties-->
-          <div class="container subheading">Faculty Advisers</div>
-          <br>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 mt-3" style="text-align: center;">
-         
-          <div class="card" style="background-color: rgb(240, 232, 232);">
+
+<!--- Faculties-->
+<div class="container subheading">Faculty Advisers</div>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 mt-3" style="text-align: center;">
+
+      <div class="card" style="background-color: rgb(240, 232, 232);">
         <img src="images/Team/image.jfif" alt="image" class="img-fa">
         <div class="card-body" >
          <h4 class="card-title">Name</h4>
          <h6 class="card-text"> Designation</h6>
-            <a href="#">abc@xyz.com</a>
-        </div>
-           </div>
-        </div>
-        
-           <div class="col-md-4 mt-3 "  style="text-align: center;">
-            
-            <div class="card " style="background-color: rgb(240, 232, 232);">
-            <img src="images/Team/image.jfif" alt="image" class="img-fa">
-            <div class="card-body">
-             <h4 class="card-title">Name</h4>
-             <h6 class="card-text"> Designation</h6>
-                <a href="#">abc@xyz.com</a>
-            </div>
-               </div>
-               </div>
-           
-               
-               <div class="col-md-4 mt-3" style="text-align: center;">
-                <div class="card" style="background-color: rgb(240, 232, 232);">
-                <img src="images/Team/image.jfif" alt="image" class="img-fa">
-                <div class="card-body">
-                 <h4 class="card-title">Name</h4>
-                    <h6 class="card-text"> Designation</h6>
-                    <a href="#">abc@xyz.com</a>
-                </div>
-                   </div>
-                   </div>
-      </div>
-      <br>
-          
-          <!---head- Volunteers-->
+         <a href="#">abc@xyz.com</a>
+       </div>
+     </div>
+   </div>
 
-          <div class="container subheading">Head Volunteers</div>
-    <br>
-    <div class="container">
- 
-          <div class="row">
-            <div class="col-md-6 mt-3"style="text-align: center;">
-          
-            <div class="card" style="background-color: rgb(240, 232, 232);">
+   <div class="col-md-4 mt-3 "  style="text-align: center;">
+
+    <div class="card " style="background-color: rgb(240, 232, 232);">
+      <img src="images/Team/image.jfif" alt="image" class="img-fa">
+      <div class="card-body">
+       <h4 class="card-title">Name</h4>
+       <h6 class="card-text"> Designation</h6>
+       <a href="#">abc@xyz.com</a>
+     </div>
+   </div>
+ </div>
+
+
+ <div class="col-md-4 mt-3" style="text-align: center;">
+  <div class="card" style="background-color: rgb(240, 232, 232);">
+    <img src="images/Team/image.jfif" alt="image" class="img-fa">
+    <div class="card-body">
+     <h4 class="card-title">Name</h4>
+     <h6 class="card-text"> Designation</h6>
+     <a href="#">abc@xyz.com</a>
+   </div>
+ </div>
+</div>
+</div>
+<br>
+
+<!---head- Volunteers-->
+
+<div class="container subheading">Head Volunteers</div>
+<br>
+<div class="container">
+
+  <div class="row">
+    <div class="col-md-6 mt-3"style="text-align: center;">
+
+      <div class="card" style="background-color: rgb(240, 232, 232);">
 
         <img src="images/Team/image2.jfif" alt="image2" class="img-d">
         <div class="card-body">
-            <h4 class="card-title">Name</h4>
-            <h6 class="card-text"> Designation</h6>
-            <a href="#">abc@xyz.com</a>
-          </div>
-          </div>
-            </div>
-          
-          
-           
-                <div class="col-md-6 mt-3"style="text-align: center;">
-                <div class="card" style="background-color: rgb(240, 232, 232);">
-                <img src="images/Team/image2.jfif" alt="image2" class="img-d">
-                <div class="card-body">
-                    <h4 class="card-title">Name</h4>
-                    <h6 class="card-text"> Designation</h6>
-                    <a href="#">abc@xyz.com</a>
-                  </div>
-                    </div>
-                  </div>
-                </div>
-   
+          <h4 class="card-title">Name</h4>
+          <h6 class="card-text"> Designation</h6>
+          <a href="#">abc@xyz.com</a>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-md-6 mt-3"style="text-align: center;">
+      <div class="card" style="background-color: rgb(240, 232, 232);">
+        <img src="images/Team/image2.jfif" alt="image2" class="img-d">
+        <div class="card-body">
+          <h4 class="card-title">Name</h4>
+          <h6 class="card-text"> Designation</h6>
+          <a href="#">abc@xyz.com</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 </div>
 
 <br>
 <br>
 
-    
 
-          
-         <!---button for  all volunteers list --> 
+
+
+<!---button for  all volunteers list --> 
 <div style="text-align: center" class="conatiner "  >
-    <a href="#"><button type="button" class="btn btn-danger btn-lg"><b><i>Volunteers </i></b></button></a>  
-    </div>
-      <br>
-      <br>
-          
-          <!---buttons for teams of different years --> 
-    <div class="conatiner"style="text-align: center"  >
-        
-    <a href="#"><button type="button" class="btn btn-danger btn-lg">Current</button></a>
-            
-    <a href="#"><button type="button" class="btn btn-danger btn-lg">2018-2019</button></a>
-                        
-     <a href="#"><button type="button" class="btn btn-danger btn-lg">2017-2018</button></a>
-                                    
-     <a href="#"><button type="button" class="btn btn-danger btn-lg">2016-2017</button></a>
-     
-    </div>
-    <br>
-    <br>
-  
-    <hr>
-    <br><br>
-          
-          <!---Our Reach --> 
-    <div class=heading-team> Our Reach</div>
-    <br>
-    <br>
+  <a href="#"><button type="button" class="btn btn-danger btn-lg"><b><i>Volunteers </i></b></button></a>  
+</div>
+<br>
+<br>
+
+<!---buttons for teams of different years --> 
+<div class="conatiner"style="text-align: center"  >
+
+  <a href="#"><button type="button" class="btn btn-danger btn-lg">Current</button></a>
+
+  <a href="#"><button type="button" class="btn btn-danger btn-lg">2018-2019</button></a>
+
+  <a href="#"><button type="button" class="btn btn-danger btn-lg">2017-2018</button></a>
+
+  <a href="#"><button type="button" class="btn btn-danger btn-lg">2016-2017</button></a>
+
+</div>
+<br>
+<br>
+
+<hr>
+<br><br>
+
+<!---Our Reach --> 
+<div class=heading-team> Our Reach</div>
+<br>
+<br>
 
 <div class="container">
-    
-    <div class="card-deck">
+
+  <div class="card-deck">
     <div class="card bg-light">
-    <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
-  
-    
-    <div class="card-body">
-      <div class="cardcontent"><b>200 </b></div>
-      <div class="cardcontent">Volunteers</div>
-        </div>
+      <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
+
+
+      <div class="card-body">
+        <div class="cardcontent"><b>200 </b></div>
+        <div class="cardcontent">Volunteers</div>
+      </div>
     </div>
     <div class=" card bg-light" >
-        <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
-        <div class="card-body">
+      <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
+      <div class="card-body">
         <div class="cardcontent"><b>1300 </b></div>
         <div class="cardcontent">Facebook Followers</div>
-            </div>
-        </div>
-        <div class="card bg-light" >
-            <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
-            <div class="card-body">
-              <div class="cardcontent"><b>100 </b></div>
-              <div class="cardcontent">Events</div>
-                </div>
-            </div>
-
+      </div>
     </div>
-    
+    <div class="card bg-light" >
+      <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
+      <div class="card-body">
+        <div class="cardcontent"><b>100 </b></div>
+        <div class="cardcontent">Events</div>
+      </div>
+    </div>
+
+  </div>
+
 </div>
 <hr>
 
 
-     <!--footer-->
-     <?php include 'includes/footer.php';?>
+<!--footer-->
+<?php include 'includes/footer.php';?>
 
 
 
@@ -255,5 +255,5 @@
 
 </body>
 
-        
- </html>
+
+</html>
