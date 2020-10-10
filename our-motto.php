cbf9de66b3d1869bc95aee0266241aee371d5bf5
Anurag <anuragjain2rr@gmail.com>
2020-10-10 16:43:40 +0530

    updated motto page

--- our-motto.php
+++ our-motto.php
@@ -4,11 +4,13 @@
    
     <!--external css-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link  rel="stylesheet" href="css/our motto.css ">
+   <link  rel="stylesheet" href="css/our motto.css ">
 
     
    <!--head php-->
     <?php include 'includes/head.php';?>
+
+
 
     
 </head>
@@ -24,75 +26,52 @@
         <!--nav-bar-->
          <?php include 'includes/navbar.php';?>
   
-  
-  
-
-
 <!--motto-->
    <!--page-name-->
   
-<div class="container-fluid pagename my-5">About NSS</div>
-   
-     <!--Motto-->
-   
-<div class="container mb-4 py-2 mt-5"  style="background-color:rgb(240, 232, 232); border: 2px solid rgb(240, 232, 232);border-radius: 7px;  box-shadow: 0 0 10px 1px rgb(21 19 43);">
-<div class="container s-heading">OUR MOTTO</div>
-<p class=" container py-3 text-m">Someone rightly quoted, "You must become purpose-driven rather than performance-driven." As committed pioneers, we inspire our volunteers to strive for excellence. They are imbued with an unrelenting zeal of never giving up but steadfastly confront any challenges of life. We encourage our volunteers to be positive, think independently, become responsible and confident. We pride ourselves creating and providing an atmosphere that would motivate, inspire, encourage and support every volunteer entrusted to our care, to turn out to be the amazing citizens. The motto of NSS family  is "Not Me but You" which has inspired and will inspire generation of youngsters to give their best for their family and our Mother India. We train our volunteers to reach out to the economically backward and the unprivileged. We motivate our volunteers to be committed individuals who have respect for all forms of life, culture and religion. The ideals enshrined in the constitution of India coincide with the same values we work towards creating a society in which all human beings can live happily and peacefully.
-</p>
- <br>
-</div>
-<br>
-   
-   <!--History-->
-   
-<div class="container mb-4 py-2 mt-5"  style="background-color:rgb(240, 232, 232); border: 2px solid rgb(240, 232, 232);border-radius: 7px;  box-shadow: 0 0 10px 1px rgb(21 19 43);">
-<div class="container s-heading">HISTORY</div>
-<p class=" container py-3 text-m"> After independence, the University Grants Commission, headed by S. Radhakrishnan, recommended the introduction of voluntary national service in academic institutions. After proper consideration and examination of the idea by the Central Advisory Board of Education (CABE) and guideline by our first PM Pt. Jawaharlal Nehru, In May 1969, a conference of student representatives (of universities and institutions of higher education) convened by the Ministry of Education and the University Grants Commission also unanimously agreed that a national-service scheme could be an instrument for national integration and orientation camp was organized at Rajghat. This camp was concluded on 7 June 1969. KK Gupta from DU was declared the first volunteer. On 24 September 1969, the then Union Education Minister V.K.R.V. Rao launched the NSS at 37 universities all states and since then the number of students strength increased from 40,000 to over 3.8 million up to the end of March 2018 students in various universities, colleges and institutions of higher learning have volunteered to take part in various community service programs.
-</p>
- <br>
-</div>
-<br>
-   
- <!--WORKS-->
-  <div class="container mb-4 py-3 mt-5"  style="background-color:rgb(240, 232, 232);border: 2px solid rgb(240, 232, 232);border-radius: 7px;    box-shadow: 0 0 10px 1px rgb(21 19 43);  ">
-        <div class=" s-heading"> OUR SIGNIFICANT WORKS</div>
-     <p>NSS in IET-DAVV was introduced in the year ______. The unit has seen active participation since then. Our volunteers aim to make their surroundings a better place to live in with their innovative ideas. Along with social welfare works, we have also organised informational talks, inviting guest speakers and engagement activities like photo walks.
-All the works that we did in recent years can be broadly categorised into :
-</p>
-        <br>
+   <div class="container-fluid pagename my-5">About NSS</div>
+
+  <div class="container about my-5 py-3">
+    <!--motto-->
+    <div class=" s-heading mt-3">OUR MOTTO</div>
+          <p class=" mx-4 py-3 text-m">Someone rightly quoted, "You must become purpose-driven rather than performance-driven." As committed pioneers, we inspire our volunteers to strive for excellence. They are imbued with an unrelenting zeal of never giving up but steadfastly confront any challenges of life. We encourage our volunteers to be positive, think independently, become responsible and confident. We pride ourselves creating and providing an atmosphere that would motivate, inspire, encourage and support every volunteer entrusted to our care, to turn out to be the amazing citizens. The motto of NSS family  is "Not Me but You" which has inspired and will inspire generation of youngsters to give their best for their family and our Mother India. We train our volunteers to reach out to the economically backward and the unprivileged. We motivate our volunteers to be committed individuals who have respect for all forms of life, culture and religion. The ideals enshrined in the constitution of India coincide with the same values we work towards creating a society in which all human beings can live happily and peacefully.</p>
+ 
+    
+<!--history-->
+        <div class=" s-heading mt-5">HISTORY</div>
+        <p class=" mx-4 py-3 text-m"> After independence, the University Grants Commission, headed by S. Radhakrishnan, recommended the introduction of voluntary national service in academic institutions. After proper consideration and examination of the idea by the Central Advisory Board of Education (CABE) and guideline by our first PM Pt. Jawaharlal Nehru, In May 1969, a conference of student representatives (of universities and institutions of higher education) convened by the Ministry of Education and the University Grants Commission also unanimously agreed that a national-service scheme could be an instrument for national integration and orientation camp was organized at Rajghat. This camp was concluded on 7 June 1969. KK Gupta from DU was declared the first volunteer. On 24 September 1969, the then Union Education Minister V.K.R.V. Rao launched the NSS at 37 universities all states and since then the number of students strength increased from 40,000 to over 3.8 million up to the end of March 2018 students in various universities, colleges and institutions of higher learning have volunteered to take part in various community service programs.</p>
+ <!--significant work-->
+        <div class=" s-heading mt-5"> OUR SIGNIFICANT WORKS</div>
+     <p class=" mx-4 py-3 text-m"> NSS in IET-DAVV was introduced in the year ______. The unit has seen active participation since then. Our volunteers aim to make their surroundings a better place to live in with their innovative ideas. Along with social welfare works, we have also organised informational talks, inviting guest speakers and engagement activities like photo walks.
+All the works that we did in recent years can be broadly categorised into :</p>
+
+   <div class="container-fluid">
+    <div class="row">
+    <!--health-->
+   <h4 class="t-heading mx-4 mt-3"> <i class="fa fa-user-md" style="font-size:25px;color:red"></i> HEALTH </h4>
+    <p class="text-m my-3 mx-4">We believe that "Health is the greatest wealth". Be it ours or others, we focus on taking good care of it. For this, we regularly conduct health checkup camps in our college premises in association with the local hospitals, practice yoga and celebrates Yoga day. We have also successfully organised blood donation drives, which was open for all. </p>
      
-  <!--health-->
-   <div class="container my-2">
-        <div class="row my-2">
-        <div class="col-md-4">
-    <h4 class="t-heading"><i class="fa fa-user-md" style="font-size:25px;color:red"></i> HEALTH</h4>
-    <P class=" container text-m my-3">We believe that "Health is the greatest wealth". Be it ours or others, we focus on taking good care of it. For this, we regularly conduct health checkup camps in our college premises in association with the local hospitals, practice yoga and celebrates Yoga day. We have also successfully organised blood donation drives, which was open for all. </P>
-    <br> </div>
-      <!--awareness-->
-   
-<div class="col-md-4">
-<h4 class="t-heading"> <i class="fa fa-users" style="font-size:25px;color:blue"></i> AWARENESS</h4>
-<P class=" container text-m my-3">"Awareness precedes change."
-Knowing the importance as well as the power of awareness, we tend to aware people around us more and more. The most effective way we have found to do so is through Nukkad Natak. We have covered topics like women safety, child marriage, malnutrition, traffic safety, climate change, de-addiction, chronic diseases in the past few years. Other ways include posters and poems.
-</P>  
-<br></div>
-
-
-        <!--Celebrations-->
-   
-        <div class="col-md-4">
-            <h4 class="t-heading"> <i class="fa fa-star"style="font-size:25px;color:gold" ></i> CELEBRATIONS</h4>
-            <P class=" container text-m my-3">Celebrations are an indispensable part of our culture. From birth anniversaries of great leaders, important days in the national history to festivals, we celebrate it all in our own unique ways. Even in the times of COVID, we managed to celebrate these important days virtually.</P>
-        </div>
-
-        <br>
     </div>
 
+    <div class="row">
+    <!--health-->
+    <h4 class="t-heading mx-4 mt-3"> <i class="fa fa-users" style="font-size:25px;color:blue"></i> AWARENESS</h4>
+     <p class="text-m my-3 mx-4">"Awareness precedes change." Knowing the importance as well as the power of awareness, we tend to aware people around us more and more. The most effective way we have found to do so is through Nukkad Natak. We have covered topics like women safety, child marriage, malnutrition, traffic safety, climate change, de-addiction, chronic diseases in the past few years. Other ways include posters and poems. </p>
+     
     </div>
 
-</div>
-<br>
-<br>
+    <div class="row">
+    <!--health-->
+    <h4 class="t-heading mx-4 mt-3"><i class="fa fa-star"style="font-size:25px;color:gold" ></i> CELEBRATIONS</h4>
+     <p class="text-m my-3 mx-4">Celebrations are an indispensable part of our culture. From birth anniversaries of great leaders, important days in the national history to festivals, we celebrate it all in our own unique ways. Even in the times of COVID, we managed to celebrate these important days virtually.</p>
+     
+    </div>
+
+  </div>
+
+
+ </div>
+   
 
 
 
