01ddeeecbcfbb36bafbc6a07ba9d83da497562e3
Anurag <anuragjain2rr@gmail.com>
2020-10-26 15:59:57 +0530

    changes in msg page

--- messages.php
+++ messages.php
@@ -5,13 +5,7 @@
     <?php include 'includes/head.php';?>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" />
-    <style>.mssg {
-        color: #000;
-        overflow-x: hidden;
-        height: 100%;
-        background-image: linear-gradient(54deg, #9164ff, #8bfff4);
-        background-repeat: no-repeat
-    }
+    <style>
 
     .card {
         box-shadow: 0px 4px 8px 0px #BDBDBD
@@ -22,75 +16,38 @@
         height: 150px;
         box-shadow: 0px 4px 8px 0px #BDBDBD
     }
-
-    .owl-carousel .owl-nav button.owl-next,
-    .owl-carousel .owl-nav button.owl-prev {
-        background: 0 0;
-        color: #1E88E5 !important;
-        border: none;
-        padding: 5px 20px !important;
-        font: inherit;
-        font-size: 50px !important
+    .msg-p{
+         min-height:730px;
+         
     }
-
-    .owl-carousel .owl-nav button.owl-next:hover,
-    .owl-carousel .owl-nav button.owl-prev:hover {
-        color: #0D47A1 !important;
-        background-color: transparent !important
+    .card{
+        border: 2px solid #212020!important;
     }
+ 
 
-    .owl-dots {
-        display: none
-    }
 
-    button:focus {
-        -moz-box-shadow: none !important;
-        -webkit-box-shadow: none !important;
-        box-shadow: none !important;
-        outline-width: 0
-    }
 
-    .item {
-        display: none
-    }
 
-    .next {
-        display: block !important;
-        position: relative;
-        transform: scale(0.8);
-        transition-duration: 0.3s;
-        opacity: 0.6
-    }
-
-    .prev {
-        display: block !important;
-        position: relative;
-        transform: scale(0.8);
-        transition-duration: 0.3s;
-        opacity: 0.6
-    }
-
-    .item.show {
-        display: block;
-        transition-duration: 0.4s
-    }
 
     .content{
         text-align: justify;
     }
 
-    @media screen and (max-width: 999px) {
+    @media (max-width:1120px){
+        .msg-p{
+         min-height:850px;
+         
+    }
+    }
+    @media (max-width:992px){
+        .msg-p{
+         min-height:auto;
+         
+    }
+    }
 
-        .next,
-        .prev {
-            transform: scale(1);
-            opacity: 1
-        }
 
-        .item {
-            display: block !important
-        }
-    }</style>
+    </style>
 
 
 
@@ -107,9 +64,8 @@
     <!--nav-bar-->
     <?php include 'includes/navbar.php';?>              
 
-    <div class="container-fluid mssg my-5">
         <div class="container-lg px-3 px-sm-5 my-5 text-center">
-            <h4 class="mb-5 font-weight-bold">Message From the Director </h4>
+            <h2 class="mb-5  heading-msg">Message From the Director </h2>
             <div class="card border-0 py-3 px-4 ">
                 <div class="row justify-content-center"> <img src="images/message/sanjiv.jpg" alt="director iet-davv" class="img-fluid profile-pic mb-4 mt-3"> </div>
                 <h5 class="mb-3 mt-2">Dr. Sanjiv Tokekar</h5>
@@ -120,34 +76,33 @@
 
         </div>   
 
-        <div class="container-fluid px-3 px-sm-5 my-5 text-center">
-            <h4 class="mb-5 font-weight-bold">Message From the Program Officers</h4>
+        <div class="container-fluid px-3 px-sm-5 my-5 text-center" style="background-color: #f8f8ff;">
+            <h2 class="mb-3 pt-4 heading-msg">Message From the Program Officers</h2>
             <div class="row">
                 <div class="col-lg-6 my-5">
-                    <div class="card border-0 py-3 px-4  ">
+                    <div class="card  msg-p border-0  px-4  ">
                         <div class="row justify-content-center"> <img src="images/message/ssk.jpg" alt="program officer boys unit" class="img-fluid profile-pic mb-4 mt-3"> </div>
                         <h5 class="mb-3 mt-2">Dr. Shailendra Singh Khinchi</h5>
                         <h6 class="mb-3 mt-2">Program Officer</h6>
                         <i class="fa fa-quote-right" style="font-size:40px;"></i>
-                        <p class="content mb-5 mx-2"> 
+                        <p class="content mb-4 mx-2"> 
                             <b>" The flower that blooms in adversity is the most rare and beautiful of all " <br><span class="d-block text-right" style="text-align: right;">-  Walt Disney.</span></b>
                             <br> The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio  and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.</p>
                         </div>
                     </div>
                     <div class="col-lg-6 my-5">
-                        <div class="card border-0 py-3 px-4 ">
+                        <div class="card  msg-p border-0  px-4 ">
                             <div class="row justify-content-center"> <img src="images/message/suwarna.jpg" alt="Dr. Suwarna Torgal program officer girls unit"class="img-fluid profile-pic mb-4 mt-3"> </div>
                             <h5 class="mb-3 mt-2">Dr. Suwarna Torgal</h5>
                             <h6 class="mb-3 mt-2">Program Officer</h6>
                             <i class="fa fa-quote-right" style="font-size:40px;"></i>
-                            <p class="content mb-5 mx-2"> "The flower that blooms in adversity is the most rare and beautiful of all" -  Walt Disney. The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio  and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.</p>
+                            <p class="content mb-4  mt-5 mx-2"> "The flower that blooms in adversity is the most rare and beautiful of all" -  Walt Disney. The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio  and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.</p>
                         </div>
                     </div>
                 </div>
 
 
             </div>
-
 
          
 
