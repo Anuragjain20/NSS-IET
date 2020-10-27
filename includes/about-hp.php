5374c447dc32a031e8707530c6dc73930e82adb6
pagare-mansee <64631013+pagare-mansee@users.noreply.github.com>
2020-10-27 10:40:29 +0530

    made changes as per required

--- includes/about-hp.php
+++ includes/about-hp.php
@@ -1,23 +1,23 @@
 <section class="enroll-section spad parallax">
   <div class="container">
    <div class="row">
-    <div class="col-md-6 mx-2" >
+    <div class="col-md-6 mx-2 mt-2"   style="background-color: rgba(255, 255, 255, 0.589)">
       <div class="row">
-       <div class="col-12 htext ">
+       <div class="col-12 htext  mt-5 ml-3 ">
         <h3 data-aos="fade-down" data-aos-duration="1000">MOTTO</h3> 
         <p class="dtext" data-aos="fade-up" data-aos-duration="1000">
           With this motto of NSS "Not Me But You", the program aims to instill the idea of social welfare in students and inspire them to provide service to society without bias. NSS volunteers work to ensure that everyone who is needy gets help to enhance their standard of living and lead a life of dignity.
         </p>
         <br> 
       </div>
-      <div class="col-12 htext">
+      <div class="col-12 htext  mt-3 ml-3 ">
        <h3 data-aos="fade-down" data-aos-duration="1000">MISSION</h3>
        <p class="dtext" data-aos="fade-up" data-aos-duration="1000"> 
          To provide a nurturing ground for holistic and integral development of future citizens who are persons of  5C's - Conscience,  Character, Compassion, Commitment and Competence.
        </p>
        <br>
      </div>
-     <div class="col-12 htext "><h3 data-aos="fade-up">VISION</h3> 
+     <div class="col-12 htext  mt-3 ml-3 "><h3 data-aos="fade-up">VISION</h3> 
        <p class="dtext" data-aos="fade-down" data-aos-duration="1000">
          Spirit lead ambassadors for a better world of equality, harmony and excellence.
        </p>
--- includes/event-index.php
+++ includes/event-index.php
@@ -1,11 +1,3 @@
-  <!--Intro-->
-<div class="container text-justify p-3" style="background-color: #f7f7f7; margin-top:30px;">
-  <h4><b> Welcome to the official website of the IET-DAVV unit of NSS.</b></h4><br>
-<h5> National Service Scheme (NSS) is a voluntary organization launch in Gandhiji's birth centenary, 1969. It is a Central Sector Scheme of Government of India,
-Ministry of Youth Affairs & Sports. 
- The program aims to promote personality development by means of participation in various government led community service activities & programs.
-</h5>
-</div>
 
 <h4 class="headingshp my-5"  data-aos="fade-in" >Activities</h4>
 
@@ -16,7 +8,7 @@ Ministry of Youth Affairs & Sports.
       <div class= "background">
         <img src="images/event-section-homepage/celebration2.jpg"class="image" style="width:50%">
         <div class="middle">
-          <a href="activities.php#celebration"><div class="text">CELEBRATIONS</div></a>
+          <a href="activities.php#celebration"><div class="text">CELEBRATIONS</h3></div></a>
         </div>
       </div>
     </div>
