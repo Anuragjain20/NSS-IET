878d362377993ff0694d2d53176f087477e0e484
Sumit Singh <singh.sumit684@gmail.com>
2020-10-27 14:17:22 +0530

    changes in home page and indentaion of all css

--- includes/about-hp.php
+++ includes/about-hp.php
@@ -1,24 +1,25 @@
 <section class="enroll-section spad parallax">
-  <div class="container">
-   <div class="row">
-    <div class="col-md-6 mx-2" >
-      <div class="row">
+  <div class="container" data-aos="fade-up" data-duration="1000">
+    <div class="row">
+      <div class="mx-2" >
+        <div class="row parallax-box">
+         <div class="col-12 htext ">
+          <h3 >MOTTO</h3> 
+          <p class="dtext" >
+            With this motto of NSS <strong>"Not Me But You"</strong>, the program aims to instill the idea of social welfare in students and inspire them to provide service to society without bias. NSS volunteers work to ensure that everyone who is needy gets help to enhance their standard of living and lead a life of dignity.
+          </p>
+          <br> 
+        </div>
+        <div class="col-12 htext">
+         <h3 >MISSION</h3>
+         <p class="dtext" > 
+           To provide a nurturing ground for holistic and integral development of future citizens who are persons of  5C's - Conscience,  Character, Compassion, Commitment and Competence.
+         </p>
+         <br>
+       </div>
        <div class="col-12 htext ">
-        <h3 data-aos="fade-down" data-aos-duration="1000">MOTTO</h3> 
-        <p class="dtext" data-aos="fade-up" data-aos-duration="1000">
-          With this motto of NSS "Not Me But You", the program aims to instill the idea of social welfare in students and inspire them to provide service to society without bias. NSS volunteers work to ensure that everyone who is needy gets help to enhance their standard of living and lead a life of dignity.
-        </p>
-        <br> 
-      </div>
-      <div class="col-12 htext">
-       <h3 data-aos="fade-down" data-aos-duration="1000">MISSION</h3>
-       <p class="dtext" data-aos="fade-up" data-aos-duration="1000"> 
-         To provide a nurturing ground for holistic and integral development of future citizens who are persons of  5C's - Conscience,  Character, Compassion, Commitment and Competence.
-       </p>
-       <br>
-     </div>
-     <div class="col-12 htext "><h3 data-aos="fade-up">VISION</h3> 
-       <p class="dtext" data-aos="fade-down" data-aos-duration="1000">
+        <h3 >VISION</h3> 
+        <p class="dtext" >
          Spirit lead ambassadors for a better world of equality, harmony and excellence.
        </p>
        <br>
--- includes/event-index.php
+++ includes/event-index.php
@@ -1,42 +1,43 @@
   <!--Intro-->
-<div class="container text-justify p-3" style="background-color: #f7f7f7; margin-top:30px;">
-  <h4><b> Welcome to the official website of the IET-DAVV unit of NSS.</b></h4><br>
-<h5> National Service Scheme (NSS) is a voluntary organization launch in Gandhiji's birth centenary, 1969. It is a Central Sector Scheme of Government of India,
-Ministry of Youth Affairs & Sports. 
- The program aims to promote personality development by means of participation in various government led community service activities & programs.
-</h5>
-</div>
+  <div class="container text-justify p-3" style="background-color: #f7f7f7; margin-top:30px;">
+    <h4 align="center"><b> Welcome to NSS IET-DAVV official website.</b></h4>
+    <br>
+    <h5> National Service Scheme (NSS) is a voluntary organization launch in Gandhiji's birth centenary, 1969. It is a Central Sector Scheme of Government of India,
+      Ministry of Youth Affairs & Sports. 
+      The program aims to promote personality development by means of participation in various government led community service activities & programs.
+    </h5>
+  </div>
 
-<h4 class="headingshp my-5"  data-aos="fade-in" >Activities</h4>
+  <h4 class="headingshp my-5"  data-aos="fade-in" >Activities</h4>
 
-<div class="container-fluid background">
-  <div class="row my-5">
-    <!-- celebration-->
-    <div class="col-sm-4 my-4" data-aos="flip-up">
-      <div class= "background">
-        <img src="images/event-section-homepage/celebration2.jpg"class="image" style="width:50%">
-        <div class="middle">
-          <a href="activities.php#celebration"><div class="text">CELEBRATIONS</div></a>
+  <div class="container-fluid background">
+    <div class="row my-5">
+      <!-- celebration-->
+      <div class="col-sm-4 my-4" data-aos="flip-up">
+        <div class= "background">
+          <img src="images/event-section-homepage/celebration2.jpg"class="image" style="width:50%">
+          <div class="middle">
+            <a href="activities.php#celebration"><div class="text">CELEBRATIONS</div></a>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Health-->
-    <div class="col-sm-4 my-4" data-aos="flip-up">
-      <div class= "background">
-        <img src="images/event-section-homepage/health2.jpg" class="image" style="width:50%" >
-        <div class="middle">
-          <a href="activities.php#health"><div class="text">HEALTH</div></a>
+      <!-- Health-->
+      <div class="col-sm-4 my-4" data-aos="flip-up">
+        <div class= "background">
+          <img src="images/event-section-homepage/health2.jpg" class="image" style="width:50%" >
+          <div class="middle">
+            <a href="activities.php#health"><div class="text">HEALTH</div></a>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Awareness-->
-    <div class="col-sm-4 my-4" data-aos="flip-up">
-      <div class= "background">
-        <img src="images/event-section-homepage/awareness 2.png" class="image" style="width:50% ">
-        <div class="middle">
-          <a href="activities.php#awareness"><div class="text">AWARENESS PROGRAMS</div></a>
+      <!-- Awareness-->
+      <div class="col-sm-4 my-4" data-aos="flip-up">
+        <div class= "background">
+          <img src="images/event-section-homepage/awareness 2.png" class="image" style="width:50% ">
+          <div class="middle">
+            <a href="activities.php#awareness"><div class="text">AWARENESS PROGRAMS</div></a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
