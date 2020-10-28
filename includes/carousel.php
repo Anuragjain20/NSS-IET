7b16832c656a6561b9c9dcc434a12921abf6c509
Sumit Singh <singh.sumit684@gmail.com>
2020-10-28 20:31:46 +0530

    added event name

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,49 +1,49 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      <!--<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>-->
-    </ol>
-    <!--images of carousel-->
-    <div class="carousel-inner">
-      <div class="carousel-item active ">
-        <img class="d-block w-100 customcarousel" src="images/carousel/carousel1.jpg" alt="First slide">
-       <div class="carousel-caption py-2">
-          <h4>Event name</h4>
-        </div>
-      </div>
-      
-      <div class="carousel-item">
-        <img class="d-block w-100 customcarousel" src="images/carousel/carousel2.jpg" alt="Second slide">
-      <div class="carousel-caption py-2 ">
-          <h4>Event name</h4>
-        </div>
-      </div>
-      
-      <div class="carousel-item">
-        <img class="d-block w-100 customcarousel" src="images/carousel/carousel3.jpg" alt="Third slide">
-       <div class="carousel-caption py-2">
-          <h4>Event name</h4>
-        </div>
-      </div>
-      
-      <div class="carousel-item">
-          <img class="d-block w-100 customcarousel" src="images/carousel/carousel4.jpg" alt="Fourth slide">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <!--<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>-->
+  </ol>
+  <!--images of carousel-->
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+      <img class="d-block w-100 customcarousel" src="images/carousel/carousel1.jpg" alt="First slide">
       <div class="carousel-caption py-2">
-          <h4>Event name</h4>
-        </div>
+        <h4>COVID-19 Awareness at Nagora Village, Dewas</h4>
       </div>
-      
+    </div>
+    
+    <div class="carousel-item">
+      <img class="d-block w-100 customcarousel" src="images/carousel/carousel2.jpg" alt="Second slide">
+      <div class="carousel-caption py-2 ">
+        <h4>COVID-19 Awareness at Nagora Village, Dewas</h4>
+      </div>
+    </div>
+    
+    <div class="carousel-item">
+      <img class="d-block w-100 customcarousel" src="images/carousel/carousel3.jpg" alt="Third slide">
+      <div class="carousel-caption py-2">
+        <h4>COVID-19 Awareness at Nagora Village, Dewas</h4>
+      </div>
+    </div>
+    
+    <div class="carousel-item">
+      <img class="d-block w-100 customcarousel" src="images/carousel/carousel4.jpg" alt="Fourth slide">
+      <div class="carousel-caption py-2">
+        <h4>COVID-19 Awareness at Nagora Village, Dewas</h4>
+      </div>
+    </div>
+    
      <!-- <div class="carousel-item">
           <img class="d-block w-100 customcarousel" src="images/carousel/carousel5.jpg" alt="Fifth slide">
       <div class="carousel-caption py-2">
-          <h4>Event name</h4>
+          <h4>COVID-19 Awareness at Nagora Village, Dewas</h4>
         </div>
       </div>-->
-   </div>
-   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
@@ -51,4 +51,4 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
-</div>
+  </div>
