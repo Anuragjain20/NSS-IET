df06446b26e25f6a7ec92d046990beea65c6f77d
Sumit Singh <singh.sumit684@gmail.com>
2020-10-29 11:28:43 +0530

    Changes in team css and icons

--- team.php
+++ team.php
@@ -59,38 +59,38 @@
 
 <!--- Program officers-->
 <div class="container-fluid">
-<div class="container subheading">Program Officers</div>
-<div class="container">
-  <div class="row my-3">
-   
-    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-     <div class="card text-center" >
+  <div class="container subheading">Program Officers</div>
+  <div class="container">
+    <div class="row my-3">
+     
+      <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+       <div class="card text-center" >
 
-      <img src="images/Team/ssk.jpg" alt="image2"class="img-s mt-3 img-fluid">
-      <div class="card-body">
-       <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
-       <h6 class="card-text"> Faculty, Applied Science Department</h6>
-       <a href="mailto:skhinchi@ietdavv.edu.in">skhinchi@ietdavv.edu.in</a>
-     </div>
-   </div>
- </div>
-
- 
- <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-   <div class="card text-center" >
-
-    <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
-    <div class="card-body">
-      <h5 class="card-title">Dr. Suwarna Torgal</h4>
-       <h6 class="card-text"> Faculty, Mechanical Engineering Department</h5>
-         <a href="mailto:storgal@ietdavv.edu.in">storgal@ietdavv.edu.in</a>
-
+        <img src="images/Team/ssk.jpg" alt="image2"class="img-s mt-3 img-fluid">
+        <div class="card-body">
+         <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
+         <h6 class="card-text"> Faculty, Applied Science Department</h6>
+         <a href="mailto:skhinchi@ietdavv.edu.in">skhinchi@ietdavv.edu.in</a>
        </div>
      </div>
    </div>
 
+   
+   <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+     <div class="card text-center" >
+
+      <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
+      <div class="card-body">
+        <h5 class="card-title">Dr. Suwarna Torgal</h4>
+         <h6 class="card-text"> Faculty, Mechanical Engineering Department</h5>
+           <a href="mailto:storgal@ietdavv.edu.in">storgal@ietdavv.edu.in</a>
+
+         </div>
+       </div>
+     </div>
+
+   </div>
  </div>
-</div>
 
 
 
@@ -101,7 +101,7 @@
 
 
 
-<!--- Faculties-->
+ <!--- Faculties-->
  <!--
 <div class="container subheading">Faculty Advisers</div>
 <br>
@@ -213,40 +213,40 @@
 
 <!---Our Reach --> 
 <div class="jumbotron jumbotron-fluid">
-<div class="subheading"> Our Reach</div>
-<br>
-<br>
+  <div class="subheading"> Our Reach</div>
+  <br>
+  <br>
 
-<div class="container">
+  <div class="container">
 
-  <div class="card-deck">
-    <div class="card bg-light">
-      <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
+    <div class="card-deck">
+      <div class="card bg-light">
+        <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
 
 
-      <div class="card-body">
-        <div class="cardcontent"><b>200 </b></div>
-        <div class="cardcontent">Volunteers</div>
+        <div class="card-body">
+          <div class="cardcontent"><b>200+ </b></div>
+          <div class="cardcontent">Volunteers</div>
+        </div>
       </div>
-    </div>
-    <div class=" card bg-light" >
-      <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
-      <div class="card-body">
-        <div class="cardcontent"><b>1300 </b></div>
-        <div class="cardcontent">Facebook Followers</div>
+      <div class=" card bg-light" >
+        <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
+        <div class="card-body">
+          <div class="cardcontent"><b>1300+ </b></div>
+          <div class="cardcontent">Facebook Followers</div>
+        </div>
       </div>
-    </div>
-    <div class="card bg-light" >
-      <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
-      <div class="card-body">
-        <div class="cardcontent"><b>100 </b></div>
-        <div class="cardcontent">Events</div>
+      <div class="card bg-light" >
+        <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
+        <div class="card-body">
+          <div class="cardcontent"><b>100+ </b></div>
+          <div class="cardcontent">Events</div>
+        </div>
       </div>
+
     </div>
 
   </div>
-
-</div>
 
 </div>
 </div>
