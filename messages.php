9a9f0e777f4dadb1828fea8622c257c50b200492
Sakshi Shastri <60570341+SAKSHISHASTRI@users.noreply.github.com>
2020-11-01 16:45:01 +0530

    Update messages.php
    Added Torgal mam 's message.

--- messages.php
+++ messages.php
@@ -83,7 +83,7 @@
 
     <!--nav-bar-->
     <?php include 'includes/navbar.php';?>              
-
+<!--
         <div class="container-lg px-3 px-sm-5 my-5 text-center">
             <h2 class="mb-5  heading-msg">Message From the Director </h2>
             <div class="card border-0 py-3 px-4 ">
@@ -96,30 +96,54 @@
             </div>
 
         </div>   
-
+-->
         <div class="container-fluid px-3 px-sm-5 my-5 text-center" style="background-color: #f8f8ff;">
             <h2 class="mb-3 pt-4 heading-msg">Message From the Program Officers</h2>
             <div class="row">
                 <div class="col-lg-6 my-5">
                     <div class="card  msg-p border-0  px-4  ">
-                        <div class="row justify-content-center"> <img src="images/message/ssk.jpg" alt="program officer boys unit" class="img-fluid profile-pic mb-4 mt-3"> </div>
+                        <div class="row justify-content-center"> <img src="images/message/ssk.jpg" alt="Dr. Shailendra Singh Khinchi, PO boys unit" class="img-fluid profile-pic mb-4 mt-3"> </div>
                         <h5 class="mb-3 mt-2">Dr. Shailendra Singh Khinchi</h5>
-                        <h6 class="mb-3 mt-2">Program Officer</h6>
+                        <h6 class="mb-3 mt-2">Program Officer, Boys Unit</h6>
                       
                         <p class="content mb-4 mx-2"> <i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true" style="color: #123c69;"></i><br>
                             <b>" The flower that blooms in adversity is the most rare and beautiful of all " <br><span class="d-block text-right" style="text-align: right;">-  Walt Disney.</span></b>
-                            <br> The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio  and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.
+                            <br> The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high 
+                            with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio
+                            and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement
+                            to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director
+                            Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.
                             <br><i class="fa fa-quote-right fa-3x fa-pull-right" aria-hidden="true" style="color: #123c69;"></i></p>
                            
                         </div>
                     </div>
+                
                     <div class="col-lg-6 my-5">
                         <div class="card  msg-p border-0  px-4 ">
-                            <div class="row justify-content-center"> <img src="images/message/suwarna.jpg" alt="Dr. Suwarna Torgal program officer girls unit"class="img-fluid profile-pic mb-4 mt-3"> </div>
+                            <div class="row justify-content-center"> <img src="images/message/suwarna.jpg" alt="Dr. Suwarna Torgal, PO girls unit"class="img-fluid profile-pic mb-4 mt-3"> </div>
                             <h5 class="mb-3 mt-2">Dr. Suwarna Torgal</h5>
-                            <h6 class="mb-3 mt-2">Program Officer</h6>
+                            <h6 class="mb-3 mt-2">Program Officer, Girls Unit</h6>
                            
-                            <p class="content mb-4  mt-5 mx-2"><i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true" style="color: #123c69;"></i><br> "The flower that blooms in adversity is the most rare and beautiful of all" -  Walt Disney. The quote speaks so much about our NSS volunteers I look at them as the flowers that dare to be different from the rest. Our dignitaries are soaring high with the promise of a bright future of our volunteers. Its uniqueness lies in its flexibility to adapt to respond to the changing perspective of social scenerio  and to be open to the new horizons  of growth and excellence. I would like to appreciate the entire team for a job well done and for their whole hearted involvement to take NSS to such great heights. I  congratulate all for  their tireless efforts and hard work for the launching of NSS IET-DAVV  website. I also thank our Director Dr. Sanjeev Tokekar for his constant support and encouragement. With warm wishes and God's blessings.
+                            <p class="content mb-4  mt-5 mx-2"><i class="fa fa-quote-left fa-3x fa-pull-left" aria-hidden="true" style="color: #123c69;"></i><br> 
+                                <b>"The larger purpose of education shall be service of Mankind".<br></b>
+                                The principles on which NSS as an organization is built, has inherent characters in line with the cause of nation building through service.
+
+                                Being  the program officer of NSS, IET-DAVV, my efforts will be towards channelizing potential of our member students to reach out to society to serve mankind.
+
+                                The team has proved its mettle in shouldering every responsibility with utmost care and diligence.
+                                I congratulate team NSS, IET-DAVV for their unending efforts to spread smiles and help the society.<br>
+
+                                I would like to give out this message that we must all remember to do things that makes life worthwhile.
+                                In the words of Dalai Lama, "The goal is not to be better than the other man, but your previous self."
+                                I hope NSS service among the youth of our country and the students prove their every action through their mindblowing achievements.
+                                Its the fantastic performance of the team NSS, IET-DAVV which is to be saluted.
+
+                                In this regard I wholeheartedly thank our Director Dr. Sanjiv Tokekar whose motivation and support is appreciated. <br>
+
+
+                                Keep shining like star.
+                              Like NSS, IET-DAVV always rocks with its performance. 
+                                Best wishes! 
                             <br><i class="fa fa-quote-right fa-3x fa-pull-right" aria-hidden="true" style="color: #123c69;"></i></p>
                         </div>
                     </div>
