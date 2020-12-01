e8e47dac8c182ee480a703affa826a7d7984c6a7
Sumit Singh <singh.sumit684@gmail.com>
2020-12-01 12:23:24 +0530

    Mansee image fix

--- team.php
+++ team.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>   
- 
-  <!--team css-->
-  <link rel="stylesheet" href="css/team.css">
 
-  <!--head php-->
-  <?php include 'includes/head.php';?>
+<head>
+
+    <!--team css-->
+    <link rel="stylesheet" href="css/team.css">
+
+    <!--head php-->
+    <?php include 'includes/head.php';?>
 
 
 </head>
+
 <body>
 
 
-  <!-- Header -->
-  <?php include 'includes/header.php';?>
-  <title>Team | National Service Scheme,IET-DAVV</title>
+    <!-- Header -->
+    <?php include 'includes/header.php';?>
+    <title>Team | National Service Scheme,IET-DAVV</title>
 
 
 
 
-  <!--nav-bar-->
-  <?php include 'includes/navbar.php';?>
+    <!--nav-bar-->
+    <?php include 'includes/navbar.php';?>
 
 
 
 
-  <!--- Team-->
-  <div class="heading-team my-5">
-   Current Team
- </div>
+    <!--- Team-->
+    <div class="heading-team my-5">
+        Current Team
+    </div>
 
- <!--- patron-->
-<!--
+    <!--- patron-->
+    <!--
  <div class="container subheading">Patron</div>
  
  <div class="container">
@@ -55,45 +57,43 @@
  
 </div>
 </div> -->
-<!-- <br> -->
+    <!-- <br> -->
 
-<!--- Program officers-->
-<div class="container-fluid">
-  <div class="container subheading">Program Officers</div>
-  <div class="container">
-    <div class="row my-3">
-     
-      <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-       <div class="card text-center" >
+    <!--- Program officers-->
+    <div class="container-fluid">
+        <div class="container subheading">Program Officers</div>
+        <div class="container">
+            <div class="row my-3">
 
-        <img src="images/Team/ssk.jpg" alt="image2"class="img-s mt-3 img-fluid">
-        <div class="card-body">
-         <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
-         <h6 class="card-text"> Faculty, Applied Science Department</h6>
-         <a href="mailto:skhinchi@ietdavv.edu.in">skhinchi@ietdavv.edu.in</a>
-       </div>
-     </div>
-   </div>
+                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                    <div class="card text-center">
 
-   
-   <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-     <div class="card text-center" >
-
-      <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
-      <div class="card-body">
-        <h5 class="card-title">Dr. Suwarna Torgal</h4>
-         <h6 class="card-text"> Faculty, Mechanical Engineering Department</h5>
-           <a href="mailto:storgal@ietdavv.edu.in">storgal@ietdavv.edu.in</a>
-
-         </div>
-       </div>
-     </div>
-
-   </div>
- </div>
+                        <img src="images/Team/ssk.jpg" alt="image2" class="img-s mt-3 img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
+                            <h6 class="card-text"> Faculty, Applied Science Department</h6>
+                            <a href="mailto:skhinchi@ietdavv.edu.in">skhinchi@ietdavv.edu.in</a>
+                        </div>
+                    </div>
+                </div>
 
 
+                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                    <div class="card text-center">
 
+                        <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Dr. Suwarna Torgal</h4>
+                                <h6 class="card-text"> Faculty, Mechanical Engineering Department
+                            </h5>
+                            <a href="mailto:storgal@ietdavv.edu.in">storgal@ietdavv.edu.in</a>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
 
 
@@ -101,8 +101,11 @@
 
 
 
- <!--- Faculties-->
- <!--
+
+
+
+        <!--- Faculties-->
+        <!--
 <div class="container subheading">Faculty Advisers</div>
 <br>
 <div class="container my-4">
@@ -147,113 +150,118 @@
 
 </div>
 -->
-<!---head- Volunteers-->
+        <!---head- Volunteers-->
 
-<div class="container subheading my-2">Head Volunteers</div>
-<div class="container my-4">
-  <div class="row my-3">
-   
-    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-     <div class="card text-center" >
+        <div class="container subheading my-2">Head Volunteers</div>
+        <div class="container my-4">
+            <div class="row my-3">
 
-      <img src="images/Team/Ravi_Kumar.jpeg" alt="Ravi_Kumar" class="img-s mt-3 img-fluid">
-      <div class="card-body">
-       <h5 class="card-title">Ravi Kumar</h5>
-       <h6 class="card-text"> Head-Boy</h6>
-       <a href="mailto:raviraj18122@gmail.com">raviraj18122@gmail.com</a>
-     </div>
-   </div>
- </div>
+                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                    <div class="card text-center">
 
- 
- <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-   <div class="card text-center" >
-
-    <img src="images/Team/Ankita_Giri.png" alt="Ankita_Giri" class="img-s mt-3 img-fluid">
-    <div class="card-body">
-      <h5 class="card-title">Ankita Giri</h4>
-       <h6 class="card-text">Head-Girl</h5>
-         <a href="mailto:iamankitagiri9@gmail.com">iamankitagiri9@gmail.com</a>
-
-       </div>
-     </div>
-   </div>
-
- </div>
-</div>
+                        <img src="images/Team/Ravi_Kumar.jpeg" alt="Ravi_Kumar" class="img-s mt-3 img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Ravi Kumar</h5>
+                            <h6 class="card-text"> Head-Boy</h6>
+                            <a href="mailto:raviraj18122@gmail.com">raviraj18122@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
 
 
-<div class="container-fluid my-3">
-<div class="container subheading">Core Leads</div>
-<div class="container my-4">
-  <div class="row my-3">
-   
-    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-     <div class="card text-center" >
+                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                    <div class="card text-center">
 
-      <img src="images/Team/Neha_Kumari.jpeg" alt="Neha Kumari" class="img-s mt-3 img-fluid">
-      <div class="card-body">
-       <h5 class="card-title">Neha Kumari</h5>
-       <h6 class="card-text">Cultural Head</h6>
-       <a href="mailto:nk733979@gmail.com">nk733979@gmail.com</a>
-     </div>
-   </div>
- </div>
+                        <img src="images/Team/Ankita_Giri.png" alt="Ankita_Giri" class="img-s mt-3 img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Ankita Giri</h4>
+                                <h6 class="card-text">Head-Girl
+                            </h5>
+                            <a href="mailto:iamankitagiri9@gmail.com">iamankitagiri9@gmail.com</a>
 
- 
- <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-   <div class="card text-center" >
-   <img src="images/Team/neerajkumarahirwar.jpeg" alt="Neeraj Kumar Ahirwar" class="img-s mt-3 img-fluid">
-    <div class="card-body">
-      <h5 class="card-title">Neeraj Kumar Ahirwar</h4>
-       <h6 class="card-text">Technical Head</h5>
-         <a href="mailto:Neeraj09kumar09@gmail.com">Neeraj09kumar09@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
 
-       </div>
+            </div>
+        </div>
 
 
-     </div>
-   </div>
+        <div class="container-fluid my-3">
+            <div class="container subheading">Core Leads</div>
+            <div class="container my-4">
+                <div class="row my-3">
 
- </div>
-</div>
+                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                        <div class="card text-center">
 
-<div class="container my-4">
-  <div class="row my-3">
-   
-    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-     <div class="card text-center" >
-     <img src="images/Team/Mansee.jpg" alt="Mansee Pagare" class="img-s mt-3 img-fluid">
-    <div class="card-body">
-      <h5 class="card-title">Mansee Pagare</h4>
-       <h6 class="card-text">Literacy Head</h5>
-         <a href="mailto:mansipagare13@gmail.com">mansipagare13@gmail.com</a>
+                            <img src="images/Team/Neha_Kumari.jpeg" alt="Neha Kumari" class="img-s mt-3 img-fluid">
+                            <div class="card-body">
+                                <h5 class="card-title">Neha Kumari</h5>
+                                <h6 class="card-text">Cultural Head</h6>
+                                <a href="mailto:nk733979@gmail.com">nk733979@gmail.com</a>
+                            </div>
+                        </div>
+                    </div>
 
-       </div>
 
-   
-   </div>
- </div>
+                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                        <div class="card text-center">
+                            <img src="images/Team/neerajkumarahirwar.jpeg" alt="Neeraj Kumar Ahirwar"
+                                class="img-s mt-3 img-fluid">
+                            <div class="card-body">
+                                <h5 class="card-title">Neeraj Kumar Ahirwar</h4>
+                                    <h6 class="card-text">Technical Head
+                                </h5>
+                                <a href="mailto:Neeraj09kumar09@gmail.com">Neeraj09kumar09@gmail.com</a>
 
- 
- <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-   <div class="card text-center" >
-   <img src="images/Team/kushlendra_gupta.jpeg" alt="Kushlendra Gupta" class="img-s mt-3 img-fluid">
-      <div class="card-body">
-       <h5 class="card-title">Kushlendra Kumar Gupta</h5>
-       <h6 class="card-text">Sports Head</h6>
-       <a href="mailto:Kushlendragupta123@gmail.com">Kushlendragupta123@gmail.com</a>
-     </div>
+                            </div>
 
-     </div>
-   </div>
 
- </div>
-</div>
+                        </div>
+                    </div>
 
-</div>
+                </div>
+            </div>
 
-<!--
+            <div class="container my-4">
+                <div class="row my-3">
+
+                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                        <div class="card text-center">
+                            <img src="images/Team/mansee.jpg" alt="Mansee Pagare" class="img-s mt-3 img-fluid">
+                            <div class="card-body">
+                                <h5 class="card-title">Mansee Pagare</h4>
+                                    <h6 class="card-text">Literacy Head
+                                </h5>
+                                <a href="mailto:mansipagare13@gmail.com">mansipagare13@gmail.com</a>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                        <div class="card text-center">
+                            <img src="images/Team/kushlendra_gupta.jpeg" alt="Kushlendra Gupta"
+                                class="img-s mt-3 img-fluid">
+                            <div class="card-body">
+                                <h5 class="card-title">Kushlendra Kumar Gupta</h5>
+                                <h6 class="card-text">Sports Head</h6>
+                                <a href="mailto:Kushlendragupta123@gmail.com">Kushlendragupta123@gmail.com</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <!--
 
 <div style="text-align: center" class="container "  >
   <a href="#"><button type="button" class="btn btn-danger btn-lg"><b><i>Volunteers </i></b></button></a>  
@@ -275,53 +283,53 @@
 </div>-->
 
 
-<hr>
+        <hr>
 
-<br>
-
-
-<!---Our Reach --> 
-<div class="jumbotron jumbotron-fluid">
-  <div class="subheading"> Our Reach</div>
-  <br>
-  <br>
-
-  <div class="container">
-
-    <div class="card-deck">
-      <div class="card bg-light">
-        <img src="images/Team/volunteer icon.png"alt="image" class=" card-img-i" >
+        <br>
 
 
-        <div class="card-body">
-          <div class="cardcontent"><b>200+ </b></div>
-          <div class="cardcontent">Volunteers</div>
+        <!---Our Reach -->
+        <div class="jumbotron jumbotron-fluid">
+            <div class="subheading"> Our Reach</div>
+            <br>
+            <br>
+
+            <div class="container">
+
+                <div class="card-deck">
+                    <div class="card bg-light">
+                        <img src="images/Team/volunteer icon.png" alt="image" class=" card-img-i">
+
+
+                        <div class="card-body">
+                            <div class="cardcontent"><b>200+ </b></div>
+                            <div class="cardcontent">Volunteers</div>
+                        </div>
+                    </div>
+                    <div class=" card bg-light">
+                        <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i">
+                        <div class="card-body">
+                            <div class="cardcontent"><b>1300+ </b></div>
+                            <div class="cardcontent">Facebook Followers</div>
+                        </div>
+                    </div>
+                    <div class="card bg-light">
+                        <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
+                        <div class="card-body">
+                            <div class="cardcontent"><b>100+ </b></div>
+                            <div class="cardcontent">Events</div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
-      </div>
-      <div class=" card bg-light" >
-        <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i" >
-        <div class="card-body">
-          <div class="cardcontent"><b>1300+ </b></div>
-          <div class="cardcontent">Facebook Followers</div>
-        </div>
-      </div>
-      <div class="card bg-light" >
-        <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
-        <div class="card-body">
-          <div class="cardcontent"><b>100+ </b></div>
-          <div class="cardcontent">Events</div>
-        </div>
-      </div>
-
     </div>
 
-  </div>
-
-</div>
-</div>
-
-<!--footer-->
-<?php include 'includes/footer.php';?>
+    <!--footer-->
+    <?php include 'includes/footer.php';?>
 
 
 
