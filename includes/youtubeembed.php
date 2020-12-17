f9fd00468420f1b537cd98ef19480ad2e38eb5f0
Anurag <anuragjain2rr@gmail.com>
2020-12-17 22:04:05 +0530

    updated image and youtube video

--- includes/youtubeembed.php
+++ includes/youtubeembed.php
@@ -7,17 +7,17 @@
   <div class="row my-5 text-center"  data-aos="fade-up">
     <div class="col-lg-4 col-12  my-4">
       <div class="container-fluid">
-        <iframe class="youtube-em" src="https://www.youtube.com/embed/mQgo6Oz4sxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="youtube-em" src="https://www.youtube.com/embed/YOMY4-ey4LQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
     <div class="col-lg-4 col-12  my-4" data-aos="fade-up">
       <div class="container-fluid">
-        <iframe class="youtube-em" src="https://www.youtube.com/embed/w5c3uY5Xmrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="youtube-em" src="https://www.youtube.com/embed/y5YBRoljiVA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
     <div class="col-lg-4 col-12 my-4" data-aos="fade-up">
       <div class="container-fluid">
-        <iframe class="youtube-em"  src="https://www.youtube.com/embed/L1D_JsoQhTg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="youtube-em"  src="https://www.youtube.com/embed/RXoVUWHrzKw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   </div>
