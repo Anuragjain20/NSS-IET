e6a49a3ba0f19be484ab24839a08ac1c8978f899
Sumit Singh <singh.sumit684@gmail.com>
2021-02-03 12:27:29 +0530

    Change in Team Head - Sakshi Shastri

--- team.php
+++ team.php
@@ -8,15 +8,15 @@
 
     <!--head php-->
     <?php include 'includes/head.php';?>
-<style>
-.pd{
-    padding-left:0px!important;
-    padding-right:0px!important;
-}
-.card{
-    max-width:380px;
-}
-</style>
+    <style>
+        .pd{
+            padding-left:0px!important;
+            padding-right:0px!important;
+        }
+        .card{
+            max-width:380px;
+        }
+    </style>
 
 </head>
 
@@ -65,34 +65,34 @@
  
 </div>
 </div> -->
-    <!-- <br> -->
+<!-- <br> -->
 
-    <!--- Program officers-->
-    <div class="container-fluid">
-        <div class="container subheading">Program Officers</div>
-        <div class="container-fluid pd">
-            <div class="row my-3 mx-auto">
+<!--- Program officers-->
+<div class="container-fluid">
+    <div class="container subheading">Program Officers</div>
+    <div class="container-fluid pd">
+        <div class="row my-3 mx-auto">
 
-                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                    <div class="card   mx-auto text-center">
+            <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                <div class="card   mx-auto text-center">
 
-                        <img src="images/Team/ssk.jpg" alt="image2" class="img-s mt-3 img-fluid">
-                        <div class="card-body">
-                            <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
-                            <h6 class="card-text"> Faculty, Applied Science Department</h6>
-                            <i class="fas fa-envelope"></i><span><a href="mailto:skhinchi@ietdavv.edu.in" target="_blank"> : skhinchi@ietdavv.edu.in</a></span>
-                        </div>
+                    <img src="images/Team/ssk.jpg" alt="image2" class="img-s mt-3 img-fluid">
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Shailendra Singh Khinchi</h5>
+                        <h6 class="card-text"> Faculty, Applied Science Department</h6>
+                        <i class="fas fa-envelope"></i><span><a href="mailto:skhinchi@ietdavv.edu.in" target="_blank"> : skhinchi@ietdavv.edu.in</a></span>
                     </div>
                 </div>
+            </div>
 
 
-                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                    <div class="card mx-auto text-center">
+            <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                <div class="card mx-auto text-center">
 
-                        <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
-                        <div class="card-body">
-                            <h5 class="card-title">Dr. Suwarna Torgal</h4>
-                                <h6 class="card-text"> Faculty, Mechanical Engineering Department
+                    <img src="images/Team/suwarna.jpg" alt="image2" class="img-s mt-3 img-fluid">
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Suwarna Torgal</h4>
+                            <h6 class="card-text"> Faculty, Mechanical Engineering Department
                             </h5>
                             <i class="fas fa-envelope"></i><span><a href="mailto:storgal@ietdavv.edu.in" target="_blank"> : storgal@ietdavv.edu.in</a></span>
 
@@ -158,38 +158,38 @@
 
 </div>
 -->
-        <!---head- Volunteers-->
+<!---head- Volunteers-->
 
-        <div class="container subheading my-2">Head Volunteers</div>
-        <div class="container-fluid pd my-4">
-            <div class="row my-3 mx-auto">
+<div class="container subheading my-2">Head Volunteers</div>
+<div class="container-fluid pd my-4">
+    <div class="row my-3 mx-auto">
 
-                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                    <div class="card  mx-auto text-center">
+        <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+            <div class="card  mx-auto text-center">
 
-                        <img src="images/Team/Ravi_Kumar.jpeg" alt="Ravi_Kumar" class="img-s mt-3 img-fluid">
-                        <div class="card-body">
-                            <h5 class="card-title">Ravi Kumar</h5>
-                            <h6 class="card-text"> Head-Boy</h6>
-                            <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/7992357264" target="_blank"> : 7992357264 </a></span><br>
-                            <i class="fas fa-envelope"></i><span><a href="mailto:raviraj18122@gmail.com" target="_blank"> : raviraj18122@gmail.com</a><span>
-                            
-                        </div>
+                <img src="images/Team/Ravi_Kumar.jpeg" alt="Ravi_Kumar" class="img-s mt-3 img-fluid">
+                <div class="card-body">
+                    <h5 class="card-title">Ravi Kumar</h5>
+                    <h6 class="card-text"> Head-Boy</h6>
+                    <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/7992357264" target="_blank"> : 7992357264 </a></span><br>
+                    <i class="fas fa-envelope"></i><span><a href="mailto:raviraj18122@gmail.com" target="_blank"> : raviraj18122@gmail.com</a><span>
+
                     </div>
                 </div>
+            </div>
 
 
-                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                    <div class="card mx-auto text-center">
+            <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                <div class="card mx-auto text-center">
 
-                        <img src="images/Team/Ankita_Giri.png" alt="Ankita_Giri" class="img-s mt-3 img-fluid">
-                        <div class="card-body">
-                            <h5 class="card-title">Ankita Giri</h5>
-                                <h6 class="card-text">Head-Girl
-                            </h6>
-                            <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/9516283013" target="_blank"> : 9516283013</a></span><br>
-                            <i class="fas fa-envelope"></i><span><a href="mailto:iamankitagiri9@gmail.com" target ="_blank"> : iamankitagiri9@gmail.com</a><span>
-                           
+                    <img src="images/developer/SAKSHI.jpg" alt="Ankita_Giri" class="img-s mt-3 img-fluid">
+                    <div class="card-body">
+                        <h5 class="card-title">Sakshi Shastri</h5>
+                        <h6 class="card-text">Head-Girl
+                        </h6>
+                        <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/9516283013" target="_blank"> : 7239923732</a></span><br>
+                        <i class="fas fa-envelope"></i><span><a href="mailto:iamankitagiri9@gmail.com" target ="_blank"> : sakshishastri076@gmail.com</a><span>
+
                         </div>
                     </div>
                 </div>
@@ -212,69 +212,69 @@
                                 <h6 class="card-text">Cultural Head</h6>
                                 <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/8084269927" target="_blank"> : 8084269927</a></span><br>
                                 <i class="fas fa-envelope"></i><span><a href="mailto:nk733979@gmail.com" target="_blank"> : nk733979@gmail.com</a><span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
 
-                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                        <div class="card  mx-auto text-center">
-                            <img src="images/Team/neerajkumarahirwar.jpeg" alt="Neeraj Kumar Ahirwar"
+                        <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                            <div class="card  mx-auto text-center">
+                                <img src="images/Team/neerajkumarahirwar.jpeg" alt="Neeraj Kumar Ahirwar"
                                 class="img-s mt-3 img-fluid">
-                            <div class="card-body">
-                                <h5 class="card-title">Neeraj Kumar Ahirwar</h4>
-                                    <h6 class="card-text">Technical Head
-                                </h5>
-                                <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/8827728108" target="_blank"> : 8827728108</a></span><br>
-                                <i class="fas fa-envelope"></i><span><a href="mailto:Neeraj09kumar09@gmail.com" target="_blank"> : neeraj09kumar09@gmail.com</a><span>
+                                <div class="card-body">
+                                    <h5 class="card-title">Neeraj Kumar Ahirwar</h4>
+                                        <h6 class="card-text">Technical Head
+                                        </h5>
+                                        <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/8827728108" target="_blank"> : 8827728108</a></span><br>
+                                        <i class="fas fa-envelope"></i><span><a href="mailto:Neeraj09kumar09@gmail.com" target="_blank"> : neeraj09kumar09@gmail.com</a><span>
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
 
                             </div>
-
-
                         </div>
-                    </div>
 
-                </div>
-            </div>
+                        <div class="container-fluid  pd my-4">
+                            <div class="row my-3 mx-auto">
 
-            <div class="container-fluid  pd my-4">
-                <div class="row my-3 mx-auto">
+                                <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                                    <div class="card mx-auto text-center">
+                                        <img src="images/Team/mansee.jpg" alt="Mansee Pagare" class="img-s mt-3 img-fluid">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Mansee Pagare</h4>
+                                                <h6 class="card-text">Literacy Head
+                                                </h5>
+                                                <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/8349739094" target ="_blank">  : 8349739094</a></span><br>
+                                                <i class="fas fa-envelope"></i><span><a href="mailto:mansipagare13@gmail.com" target="_blank"> : mansipagare13@gmail.com</a><span>
 
-                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                        <div class="card mx-auto text-center">
-                            <img src="images/Team/mansee.jpg" alt="Mansee Pagare" class="img-s mt-3 img-fluid">
-                            <div class="card-body">
-                                <h5 class="card-title">Mansee Pagare</h4>
-                                    <h6 class="card-text">Literacy Head
-                                </h5>
-                                <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/8349739094" target ="_blank">  : 8349739094</a></span><br>
-                                <i class="fas fa-envelope"></i><span><a href="mailto:mansipagare13@gmail.com" target="_blank"> : mansipagare13@gmail.com</a><span>
-
-                            </div>
+                                                </div>
 
 
-                        </div>
-                    </div>
+                                            </div>
+                                        </div>
 
 
-                    <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
-                        <div class="card mx-auto text-center">
-                            <img src="images/Team/kushlendra_gupta.jpeg" alt="Kushlendra Gupta"
-                                class="img-s mt-3 img-fluid">
-                            <div class="card-body">
-                                <h5 class="card-title">Kushlendra Kumar Gupta</h5>
-                                <h6 class="card-text">Sports Head</h6>
-                                <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/9407323164" target="_blank"> : 9407323164</a></span><br>
-                                <i class="fas fa-envelope"></i><span><a href="mailto:Kushlendragupta123@gmail.com" target="_blank"> : kushlendragupta123@gmail.com</a><span>
-                            </div>
+                                        <div class=" col-12 col-sm-8 col-md-7 col-lg-5 mx-auto my-3">
+                                            <div class="card mx-auto text-center">
+                                                <img src="images/Team/kushlendra_gupta.jpeg" alt="Kushlendra Gupta"
+                                                class="img-s mt-3 img-fluid">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Kushlendra Kumar Gupta</h5>
+                                                    <h6 class="card-text">Sports Head</h6>
+                                                    <i class="fab fa-whatsapp"></i><span class="card-text"><a href ="https://wa.me/9407323164" target="_blank"> : 9407323164</a></span><br>
+                                                    <i class="fas fa-envelope"></i><span><a href="mailto:Kushlendragupta123@gmail.com" target="_blank"> : kushlendragupta123@gmail.com</a><span>
+                                                    </div>
 
-                        </div>
-                    </div>
+                                                </div>
+                                            </div>
 
-                </div>
-            </div>
+                                        </div>
+                                    </div>
 
-        </div>
+                                </div>
 
         <!--
 
@@ -298,53 +298,53 @@
 </div>-->
 
 
-        <hr>
+<hr>
 
-        <br>
-
-
-        <!---Our Reach -->
-        <div class="jumbotron jumbotron-fluid">
-            <div class="subheading"> Our Reach</div>
-            <br>
-            <br>
-
-            <div class="container">
-
-                <div class="card-deck">
-                    <div class="card bg-light">
-                        <img src="images/Team/volunteer icon.png" alt="image" class=" card-img-i">
+<br>
 
 
-                        <div class="card-body">
-                            <div class="cardcontent"><b>200+ </b></div>
-                            <div class="cardcontent">Volunteers</div>
-                        </div>
-                    </div>
-                    <div class=" card bg-light">
-                        <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i">
-                        <div class="card-body">
-                            <div class="cardcontent"><b>1300+ </b></div>
-                            <div class="cardcontent">Facebook Followers</div>
-                        </div>
-                    </div>
-                    <div class="card bg-light">
-                        <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
-                        <div class="card-body">
-                            <div class="cardcontent"><b>100+ </b></div>
-                            <div class="cardcontent">Events</div>
-                        </div>
-                    </div>
+<!---Our Reach -->
+<div class="jumbotron jumbotron-fluid">
+    <div class="subheading"> Our Reach</div>
+    <br>
+    <br>
 
+    <div class="container">
+
+        <div class="card-deck">
+            <div class="card bg-light">
+                <img src="images/Team/volunteer icon.png" alt="image" class=" card-img-i">
+
+
+                <div class="card-body">
+                    <div class="cardcontent"><b>200+ </b></div>
+                    <div class="cardcontent">Volunteers</div>
                 </div>
-
+            </div>
+            <div class=" card bg-light">
+                <img src="images/Team/graph icon.png" alt="image2" class=" card-img-i">
+                <div class="card-body">
+                    <div class="cardcontent"><b>1300+ </b></div>
+                    <div class="cardcontent">Facebook Followers</div>
+                </div>
+            </div>
+            <div class="card bg-light">
+                <img src="images/Team/events icon.png" alt="image2" class=" card-img-i">
+                <div class="card-body">
+                    <div class="cardcontent"><b>100+ </b></div>
+                    <div class="cardcontent">Events</div>
+                </div>
             </div>
 
         </div>
+
     </div>
 
-    <!--footer-->
-    <?php include 'includes/footer.php';?>
+</div>
+</div>
+
+<!--footer-->
+<?php include 'includes/footer.php';?>
 
 
 
